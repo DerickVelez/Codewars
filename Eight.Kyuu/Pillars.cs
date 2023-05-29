0679aa6a0924b7f7e35b27da3ba5961f5d3b5734
Jan Derick Velez <jderickvelez@gmail.com>
2023-05-29 14:53:40 +0800

    Add project files.

---
+++ Eight.Kyuu/Pillars.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eight.Kyuu
+{
+    
+    public class Pillars
+    {
+        public static int ReturnPillars(int numPill, int dist, int width)
+        {
+            if (numPill <= 1 && dist >= 10 && dist <= 30 && width >= 10 && width <= 50)
+            { return 0; }
+            else
+                return ((numPill - 2) * width) + ((numPill - 1) * (dist * 100));
+        }
+    }
+}
---
+++ EightKyu.Test/PillarsTest.cs
@@ -0,0 +1,24 @@
+﻿using Eight.Kyuu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EightKyu.Test
+{
+    public class PillarsTest
+    {
+        [Theory]
+        [InlineData(11, 15, 30, 15270)]
+        public void PillarsTestshouldreturnDist(int numPill, int dist, int width, int expected)
+        {
+
+            var result = Pillars.ReturnPillars(numPill,dist,width);
+
+            Assert.Equal(expected,result);
+
+        }
+           
+    }
+}
