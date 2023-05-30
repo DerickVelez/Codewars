7b0f5fe04ef2268c595e9172a850ed991416f412
Jan Derick Velez <jderickvelez@gmail.com>
2023-05-30 11:24:41 +0800

    Added SevenKyu Kata: Sum of a Sequence

---
+++ Seven.Kyu/SumofaSequence.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seven.Kyu
+{
+    public static class SumofaSequence
+    {
+        public static int SequenceSum(int start, int end, int step)
+        {
+            var added = 0;
+
+            if (start > end) { return 0; }
+
+            for (int i = start; i <= end; i += step)
+            {
+                added += i;
+            }
+            return added;
+        }
+    }
+}
---
+++ SevenKyu.Test/SumofaSequenceTest.cs
@@ -0,0 +1,22 @@
+﻿using Seven.Kyu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SevenKyu.Test
+{
+    public class SumofaSequenceTest
+    {
+        [Theory]
+        [InlineData(2, 6, 2, 12)]
+        [InlineData(1, 15, 3, 35)]
+        [InlineData(16, 15, 3, 0)]
+        public void ShouldReturnSum(int start,int end,int steps,int expected)
+        {
+            var result = SumofaSequence.SequenceSum(start, end, steps);
+            Assert.Equal(expected, result);
+        }
+    }
+}
