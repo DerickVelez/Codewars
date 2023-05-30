71af985c6fde95ec5750c3f238e617f4fd24152b
Jan Derick Velez <jderickvelez@gmail.com>
2023-05-30 09:23:16 +0800

    Added Average Solution Kata

---
+++ Eight.Kyuu/AverageSolution.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eight.Kyuu
+{
+    public static class AverageSolution
+    {
+        public static double FindAverage(double[] array)
+        {
+            if (array.Length <= 0) { return 0; }
+
+
+            double ave = array.Sum() / (array.Length);
+            return ave;
+        }
+    }
+}
