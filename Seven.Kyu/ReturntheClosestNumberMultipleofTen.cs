79731b881c1067ab1cdd0da8e04f613f23a236b7
Jan Derick Velez <jderickvelez@gmail.com>
2023-05-31 16:53:47 +0800

    Added Seven Kyu: Return the Closest Number Multiple of Ten

---
+++ Seven.Kyu/ReturntheClosestNumberMultipleofTen.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seven.Kyu
+{
+    public class ReturntheClosestNumberMultipleofTen
+    {
+        public static int ClosestMultiple10(int num)
+        {
+            Console.WriteLine(num);
+            return (int)Math.Round(num / 10.000, MidpointRounding.AwayFromZero) * 10;
+
+        }
+    }
+}
---
+++ SevenKyu.Test/ReturntheClosestNumberMultipleofTenTest.cs
@@ -0,0 +1,21 @@
+﻿using Seven.Kyu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SevenKyu.Test
+{
+    public class ReturntheClosestNumberMultipleofTenTest
+    {
+        [Theory]
+        [InlineData(12785, 12790)]
+        [InlineData(2344,2340)]
+        public void ShouldReturnMultipleofTen(int num, int expected)
+        {
+            var result = ReturntheClosestNumberMultipleofTen.ClosestMultiple10(num);
+            Assert.Equal(expected, result);
+        }
+    }
+}
