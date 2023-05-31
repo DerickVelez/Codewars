426ebbd65507c311259b9903e050fdcb259f0d03
Jan Derick Velez <jderickvelez@gmail.com>
2023-05-31 08:06:15 +0800

    Added Seven Kyu Kata: Product of Largest Pair

---
+++ Seven.Kyu/ProductofLargestPair.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seven.Kyu
+{
+    public static class ProductofLargestPair
+    {
+        public static int MaxProduct(int[] array)
+        {
+            Array.Sort(array);
+            return array[array.Length - 1] * array[array.Length - 2];
+        }
+    }
+}
---
+++ SevenKyu.Test/ProductofLargestPairTest.cs
@@ -0,0 +1,22 @@
+﻿using Seven.Kyu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SevenKyu.Test
+{
+    public class ProductofLargestPairTest
+    {
+        [Theory]
+        [InlineData(new int[] { 39, 135, 47, 275, 37, 108, 265, 457, 2, 133, 316, 330, 153, 253, 321, 411 }, 187827)]
+        [InlineData(new int[] { 375, 56, 337, 466, 203 }, 174750)]
+
+        public void ShouldReturnLargestPairProduct(int[] array, int expected)
+        {
+            var result = ProductofLargestPair.MaxProduct(array);
+            Assert.Equal(expected, result);
+        }
+    }
+}
