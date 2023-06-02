88f877b9edc5a4f593fe92973ab974dcc8303881
Jan Derick Velez <jderickvelez@gmail.com>
2023-06-02 22:03:32 +0800

    Added Eight Kyu Kata:Count the number of cubes with paint on

---
+++ Eight.Kyuu/Countthenumberofcubeswithpainton.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eight.Kyuu
+{
+    public static class Countthenumberofcubeswithpainton
+    {
+        public static int CountSquares(int cuts)
+        {
+            if (cuts == 0)
+            {
+                return 1;
+            }
+            int side1 = ((cuts + 1) * (cuts + 1)) * 2;
+            int side2 = (cuts - 1) * (((cuts + 1) * 4) - 4);
+            return side1 + side2;
+        }
+    }
+}
---
+++ EightKyu.Test/CountthenumberofcubeswithpaintonTest.cs
@@ -0,0 +1,23 @@
+﻿using Eight.Kyuu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EightKyu.Test
+{
+    public class CountthenumberofcubeswithpaintonTest
+    {
+        [Theory]
+        [InlineData(0, 1)]
+        [InlineData(16,1538)]
+
+        public void ShouldReturnnumberofCubes(int result,int expected)
+        {
+            var output = Countthenumberofcubeswithpainton.CountSquares(result);
+            Assert.Equal(expected, output);
+
+        }
+    }
+}
