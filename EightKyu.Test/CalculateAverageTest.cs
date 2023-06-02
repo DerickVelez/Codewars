f81825e95a92ab1e2604c9fdd1e5131f02c45894
Jan Derick Velez <jderickvelez@gmail.com>
2023-06-02 22:17:33 +0800

    Added Eight Kyu Kata: Calculate average

---
+++ EightKyu.Test/CalculateAverageTest.cs
@@ -0,0 +1,22 @@
+﻿using Eight.Kyuu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EightKyu.Test
+{
+    public class CalculateAverageTest
+    {
+        [Theory]
+        [InlineData(new double[] { 17, 16, 16, 16, 16, 15, 17, 17, 15, 5, 17, 17, 16 }, 200.0 / 13.0)]
+
+        public void ShouldReturnAverage(double[] input, double expected)
+        {
+            var result = CalculateAverage.FindAverage(input);
+            Assert.Equal(expected, result);
+
+        }
+    }
+}
