7519b6f68fd99d7536e6a821a3d3134ceeed548b
Jan Derick Velez <jderickvelez@gmail.com>
2023-06-07 22:52:59 +0800

    Added Eigth Kyu Kata: Array plus Array

---
+++ Eight.Kyuu/ArrayPlusArray.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eight.Kyuu
+{
+    public static class ArrayPlusArray
+    {
+        public static int SumofArray(int[] arr1, int[] arr2) => arr1.Sum() + arr2.Sum();
+    }
+}
---
+++ EightKyu.Test/ArrayPlusArrayTest.cs
@@ -0,0 +1,24 @@
+﻿using Eight.Kyuu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EightKyu.Test
+{
+    public class ArrayPlusArrayTest
+    {
+        [Theory]
+        [InlineData(new int[] { 1, 2, 3 }, new int[] { 4, 5, 6 }, 21)]
+
+
+        public void ShouldReturnSum(int[] arr1, int[] arr2, int expected)
+        {
+            var result = ArrayPlusArray.SumofArray(arr1, arr2);
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
+
