66c2287827c97a7c23910698c5b9a1dad0b11a72
Jan Derick Velez <jderickvelez@gmail.com>
2023-06-07 22:28:28 +0800

    Added Seven Kyu Kata: Cats and Shelves

---
+++ Seven.Kyu/CatsAndShelves.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seven.Kyu
+{
+    public static class CatsAndShelves
+    {
+        public static int Cats(int start, int finish)
+        {
+            int difference = finish - start;
+            return (int)Math.Floor((double)difference / 3) + difference % 3;
+        }
+    }
+}
---
+++ SevenKyu.Test/CatsAndShelvesTest.cs
@@ -0,0 +1,22 @@
+﻿using Seven.Kyu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SevenKyu.Test
+{
+    public class CatsAndShelvesTest
+    {
+        [Theory]
+        [InlineData (1, 5, 2)]
+        [InlineData(2, 4, 2)]
+
+        public void ShouldReturnSteps(int start,int finish,int expected)
+        {
+            var result = CatsAndShelves.Cats(start, finish);
+            Assert.Equal(expected,result);
+        }
+    }
+}
