9f0a28c889c03493654f614e09d92f3e9a19f704
Jan Derick Velez <jderickvelez@gmail.com>
2023-06-08 23:33:02 +0800

    Added Eight Kyu Kata: Reverse Sequence

---
+++ Eight.Kyuu/ReversedSequence.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eight.Kyuu
+{
+    public static class ReversedSequence
+    {
+        public static int[] ReverseSeq(int n)
+        {
+            List<int> numbers = new List<int>();
+
+            while (n > 0)
+            {
+                numbers.Add(n);
+                n--;
+            }
+            return numbers.ToArray();
+        }
+    }
+}
---
+++ EightKyu.Test/ReversedSequenceTest.cs
@@ -0,0 +1,21 @@
+﻿using Eight.Kyuu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EightKyu.Test
+{
+    public class ReversedSequenceTest
+    {
+        [Theory]
+        [InlineData(5, new int[] { 5, 4, 3, 2, 1 })]
+
+        public void ShouldReturnReversedArray(int n, int[] expected)
+        {
+            var result = ReversedSequence.ReverseSeq(n);
+            Assert.Equal(expected, result);
+        }
+    }
+}
