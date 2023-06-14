ab8e1b7f0021e0d87f0c33eb9e41b06b4c8a09ee
Jan Derick Velez <jderickvelez@gmail.com>
2023-06-14 11:01:30 +0800

    Added Eight Kyu Kata: Multiple of Index

---
+++ Eight.Kyuu/MultipleOfIndex.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eight.Kyuu
+{
+    public static class MultipleOfIndex
+    {
+        public static int[] MultipleOfIndeces(int[] xs)
+        {
+            List<int> res = xs.ToList();
+            List<int> result = new List<int>();
+
+
+            for (int i = 0; i < res.Count; i++)
+            {
+
+                if ((xs[i] == 0 && i == 0) || (i != 0 && xs[i] % i == 0))
+                {
+                    result.Add(xs[i]);
+                }
+
+            }
+
+        
+
+            return result.ToArray();
+        }
+    }
+}
---
+++ EightKyu.Test/MultipleOfIndexTest.cs
@@ -0,0 +1,21 @@
+﻿using Eight.Kyuu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EightKyu.Test
+{
+    public class MultipleOfIndexTest
+    {
+        [Theory]
+        [InlineData(new int[] { 22, -6, 32, 82, 9, 25 }, new int[] { -6, 32, 25 })]
+
+        public static void ShouldReturnIndeces(int[] input, int[] expected)
+        {
+            var result = MultipleOfIndex.MultipleOfIndeces(input);
+            Assert.Equal(expected, result);
+        }
+    }
+}
