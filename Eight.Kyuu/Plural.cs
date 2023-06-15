9a115a82482ac32f8bf01b72927b8565818f7f73
Jan Derick Velez <jderickvelez@gmail.com>
2023-06-15 08:23:04 +0800

    Added Eight Kyu Kata: Plural

---
+++ Eight.Kyuu/Plural.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eight.Kyuu
+{
+    public class Plural
+    {
+        public static bool PluralWords(double n, double x = 1) => n == x ? false : true;
+    
+    }
+}
---
+++ EightKyu.Test/PluralTest.cs
@@ -0,0 +1,23 @@
+﻿using Eight.Kyuu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EightKyu.Test
+{
+    public class PluralTest
+    {
+        [Theory]
+        [InlineData(0, true)]
+        [InlineData(1, false)]
+        [InlineData(double.PositiveInfinity, true)]
+
+        public void ShouldReturnTrueifPlural(double input,bool expected)
+        {
+            var result = Plural.PluralWords(input);
+            Assert.Equal(expected, result);
+        }
+    }
+}
