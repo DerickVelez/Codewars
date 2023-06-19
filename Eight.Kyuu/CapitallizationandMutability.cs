7e023f7e744e2ded5bde90ce9b1237eaa3d176a2
Jan Derick Velez <jderickvelez@gmail.com>
2023-06-19 13:40:36 +0800

    Added Eight Kyu Kata: CapitalizationandMutability

---
+++ Eight.Kyuu/CapitallizationandMutability.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Eight.Kyuu
+{
+    public class CapitallizationandMutability
+    {
+        public static string CapitalizeWord(string word)
+        {
+            return CultureInfo.CurrentCulture.TextInfo.ToTitleCase(word);
+        }
+    }
+}
---
+++ EightKyu.Test/CapitalizationandMutabilityTest.cs
@@ -0,0 +1,22 @@
+﻿using Eight.Kyuu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EightKyu.Test
+{
+    public class CapitalizationandMutabilityTest
+    {
+        [Theory]
+        [InlineData("Word", "word")]
+
+        public void ShouldReturnTitleCase(string expected, string input)
+        {
+            var result = CapitallizationandMutability.CapitalizeWord(input);
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
