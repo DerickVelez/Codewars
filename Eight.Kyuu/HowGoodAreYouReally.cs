99f6031a5a48ebcec7cd736fe268a3309c90703e
Jan Derick Velez <jderickvelez@gmail.com>
2023-06-22 10:43:56 +0800

    Added Eight Kyu Kata: How Good are you really?

---
+++ Eight.Kyuu/HowGoodAreYouReally.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eight.Kyuu
+{
+    public static class HowGoodAreYouReally
+    {
+        public static bool BetterThanAverage(int[] ClassPoints, int YourPoints)
+        {
+            var ave = ClassPoints.Sum() / ClassPoints.Count();
+
+            if (YourPoints > ave)
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+    }
+}
---
+++ EightKyu.Test/HowGoodAreYouReallyTest.cs
@@ -0,0 +1,21 @@
+﻿using Eight.Kyuu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EightKyu.Test
+{
+    public class HowGoodAreYouReallyTest
+    {
+        [Theory]
+        [InlineData(new int[] { 100, 40, 34, 57, 29, 72, 57, 88 }, 75, true) ]
+
+        public void ShouldReturnTrueIfBetterthanAve(int[] ClassPoint, int YourPoints, bool expected)
+        {
+            var result = HowGoodAreYouReally.BetterThanAverage(ClassPoint, YourPoints);
+            Assert.Equal(expected, result);
+        }
+    }
+}
