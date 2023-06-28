34206250709081addaac1f81e048ec2f3d257176
Jan Derick Velez <jderickvelez@gmail.com>
2023-06-28 23:00:31 +0800

    Added Seven Kyu Kata: Sort Array by String Length

---
+++ Seven.Kyu/SortArraybyStringLength.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seven.Kyu
+{
+    public class SortArraybyStringLength
+    {
+        public static string[] SortByLength(string[] array)
+        {
+            string secArr;
+            bool isSorted = false;
+            while (isSorted == false)
+            {
+                isSorted = true;
+                for (int i = 0; i < array.Length - 1; i++)
+                {
+                    if (array[i].Length > array[i + 1].Length)
+                    {
+                        secArr = array[i + 1];
+                        array[i + 1] = array[i];
+                        array[i] = secArr;
+                        isSorted = false;
+                    }
+
+                }
+            }
+            return array;
+        }
+    }
+}
---
+++ SevenKyu.Test/SortArraybyStringLengthTest.cs
@@ -0,0 +1,24 @@
+﻿using Seven.Kyu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SevenKyu.Test
+{
+    public class SortArraybyStringLengthTest
+    {
+        [Theory]
+        [InlineData(new string[] { "I", "To", "Beg", "Life" }, new string[] { "Beg", "Life", "I", "To" })]
+    
+        public void ShouldReturnSortedArray(string[] expected, string[] input)
+        {
+            var result = SortArraybyStringLength.SortByLength(input);
+
+            Assert.Equal(expected, result);
+        }
+
+    }
+
+}
