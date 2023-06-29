35df520f6ea9a1085d13c1d7e0aa27dea3f653bf
Jan Derick Velez <jderickvelez@gmail.com>
2023-06-29 22:00:32 +0800

    Added Eight Kyu Kata: Convert String to Array

---
+++ Eight.Kyuu/ConvertStringtoArray.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eight.Kyuu
+{
+    public class ConvertStringtoArray
+    {
+        public static string[] StringToArray(string str) => str.Split();
+    }
+}
+    
---
+++ EightKyu.Test/ConvertStringtoArrayTest.cs
@@ -0,0 +1,22 @@
+﻿using Eight.Kyuu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EightKyu.Test
+{
+    public class ConvertStringtoArrayTest
+    {
+        [Theory]
+        [InlineData(new string[] { "Robin", "Singh" }, "Robin Singh")]
+        
+
+        public void ShouldReturnArray(string[] expected, string input)
+        {
+            var result =  ConvertStringtoArray.StringToArray(input);
+            Assert.Equal(expected, result); 
+        }
+    }
+}
