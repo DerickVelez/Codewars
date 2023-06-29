cef9553e734c8cad841ca3a6c1355ef34354abfb
Jan Derick Velez <jderickvelez@gmail.com>
2023-06-30 06:53:53 +0800

    Added Eight Kyu Kata: Lario and Luigi Pipe Problem

---
+++ EightKyu.Test/LarioandLuigiPipeProblemTest.cs
@@ -0,0 +1,21 @@
+﻿using Eight.Kyuu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EightKyu.Test
+{
+    public class LarioandLuigiPipeProblemTest
+    {
+        [Theory]
+        [InlineData(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 }, new int[] { 1, 2, 3, 5, 6, 8, 9 })]
+
+        public void ShouldReturnResult(int[] expected, int[] input)
+        {
+            var result = LarioandLuigiPipeProblem.PipeFix(input);
+            Assert.Equal(expected, result);
+        }
+    }
+}
