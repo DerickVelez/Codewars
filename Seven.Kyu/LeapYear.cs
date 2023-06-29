61b0a7e7d4d9922cf2068f0a56ad6dcb2c7199a6
Jan Derick Velez <jderickvelez@gmail.com>
2023-06-29 21:54:00 +0800

    Added Seven Kyu Kata:Leap Years

---
+++ Seven.Kyu/LeapYear.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seven.Kyu
+{
+      public class LeapYear
+    {
+        public static bool IsLeapYear(int year)
+        {
+            if (year % 400 == 0)
+            { return true; }
+            if (year % 100 == 0)
+            { return false; }
+            if (year % 4 == 0)
+            { return true; }
+            else
+                return false;
+        }
+    }
+}
---
+++ SevenKyu.Test/LeapYearsTest.cs
@@ -0,0 +1,22 @@
+﻿using Seven.Kyu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SevenKyu.Test
+{
+    public class LeapYearsTest
+    {
+        [Theory]
+        [InlineData(true,1600)]
+        [InlineData(false, 2200)]
+        
+        public void ShouldReturnLeapYear(bool expected,  int year)
+        {
+            var result = LeapYear.IsLeapYear(year);
+            Assert.Equal(expected, result);
+        }
+    }
+}
