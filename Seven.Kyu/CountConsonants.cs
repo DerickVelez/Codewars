0c95b3c604cf0b2a121a97b06bb617b29b2a7a30
Jan Derick Velez <jderickvelez@gmail.com>
2023-06-30 06:38:01 +0800

    Added Seven Kyu Kata: Count Consonants

---
+++ Seven.Kyu/CountConsonants.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seven.Kyu
+{
+    public class CountConsonants
+    {
+        public static int ConsonantCount(string str)
+        {
+            int len = str.Length;
+            int consonants = 0;
+            int vowel = 0;
+
+            for (int i = 0; i < len; i++)
+            {
+                if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u' || str[i] == 'A' || str[i] == 'E' || str[i] == 'I' || str[i] == 'O' || str[i] == 'U')
+                {
+                    vowel++;
+                }
+                else if ((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z'))
+                {
+                    consonants++;
+                }
+
+            }
+            return consonants;
+        }
+    }
+}
---
+++ SevenKyu.Test/CountConsonantsTest.cs
@@ -0,0 +1,22 @@
+﻿using Seven.Kyu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SevenKyu.Test
+{
+   public class CountConsonantsTest
+    {
+        [Theory]
+        [InlineData(5, "Bbbbb")]
+        [InlineData(7, "h^$&^#$&^elLo world")]
+
+        public void ShouldReturnNumberofConsonants(int expected,  string actual)
+        {
+            var result = CountConsonants.ConsonantCount(actual);
+            Assert.Equal(expected, result);
+        }
+    }
+}
