85c57fb4010739e2f585d293f572bb19ab3eb9af
Jan Derick Velez <jderickvelez@gmail.com>
2023-07-04 16:22:08 +0800

    Added Eight Kyu Kata: No Zero for Heroes

---
+++ Eight.Kyuu/NoZerosforHeroes.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eight.Kyuu
+{
+    public class NoZerosforHeroes
+    {
+
+        public static int NoBoringZeros(int n)
+        {
+            string numberString = n.ToString();
+            var EndIndex = numberString.Length - 1;
+
+            List<string> digits = new List<string> { "1" };
+            int i = EndIndex;
+
+            if (n == 0)
+            { return n; }
+            else
+
+                while (numberString[i] == '0')
+                {
+                    digits.Add("0");
+                    i--;
+                }
+
+            string numbers = string.Join("", digits);
+            int result = Int32.Parse(numbers);
+            return n / result;
+        }
+    }
+}
---
+++ EightKyu.Test/NoZerosforHeroesTest.cs
@@ -0,0 +1,23 @@
+﻿using Eight.Kyuu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EightKyu.Test
+{
+    public class NoZerosforHeroesTest
+    {
+        [Theory]
+        [InlineData(145, 1450)]
+        [InlineData(0, 0)]
+        [InlineData(-105, -1050)]
+
+        public void ShouldReturnNoZeroEnding(int expected, int actual)
+        {
+            var result = NoZerosforHeroes.NoBoringZeros(actual); 
+            Assert.Equal(expected, result);
+        }
+    }
+}
