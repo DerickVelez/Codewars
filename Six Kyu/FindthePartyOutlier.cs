e2a4a6f49db0493e3a7b42947913a1a5cf1ae554
Jan Derick Velez <jderickvelez@gmail.com>
2023-07-13 16:58:03 +0800

    Added Six Kyu Kata: Find the Party Outlier

---
+++ Six Kyu/FindthePartyOutlier.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Six_Kyu
+{
+    public class FindthePartyOutlier
+    {
+        public static int Find(int[] integers)
+        {
+            int oddCounter = 0;
+            int evenCounter = 0;
+            int odd = 0;
+            int even = 0;
+
+            while (oddCounter < 2 || evenCounter < 2)
+            {
+                foreach (int digits in integers)
+                {
+                    if (digits % 2 == 0)
+                    {
+                        evenCounter++;
+                        even = even = +digits;
+                    }
+                    else
+                    {
+                        oddCounter++;
+                        odd = odd = +digits;
+                    }
+                }
+            }
+
+            if (oddCounter > evenCounter)
+            { return even; }
+            else
+            { return odd; }
+
+        }
+    }
+}
---
+++ SixKyu.Test/FindthePartyOutlierTest.cs
@@ -0,0 +1,23 @@
+﻿using Six_Kyu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SixKyu.Test
+{
+    public class FindthePartyOutlierTest
+    {
+        [Theory]
+        [InlineData(206847684 , new int[] { 206847684, 1056521, 7, 17, 1901, 21104421, 7, 1, 35521, 1, 7781 })]
+        [InlineData(3 ,new int[] { 2, 6, 8, -10, 3 })]
+        
+        public void ShouldReturnOutlier(int expected, int[] outlier)
+        {
+            var result = FindthePartyOutlier.Find(outlier);
+            Assert.Equal(expected, result); 
+        }
+
+    }
+}
