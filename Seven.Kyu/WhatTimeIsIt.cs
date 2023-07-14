538e701fc402dfc159d21282fd1642e2c16e58bd
Jan Derick Velez <jderickvelez@gmail.com>
2023-07-14 23:36:08 +0800

    Added Seven Kyu Kata: What Time is it?

---
+++ Seven.Kyu/WhatTimeIsIt.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seven.Kyu
+{
+    public class WhatTimeIsIt
+    {
+        public string GetMilitaryTimeFromStandardTime(string time)
+        {
+
+            DateTime dateTime;
+            if (DateTime.TryParse(time, out dateTime))
+            {
+                return dateTime.ToString("HH:mm:ss");
+            }
+            return time;
+        }
+
+    }
+}
---
+++ SevenKyu.Test/WhatTimeIsItTest.cs
@@ -0,0 +1,23 @@
+﻿using Seven.Kyu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SevenKyu.Test
+{
+    public class WhatTimeIsItTest
+    {
+        [Theory]
+        [InlineData("23:46:47", "11:46:47PM")]
+        [InlineData("22:44:45", "10:44:45PM")]
+
+        public void ShouldReturnMilitaryTime(string expected, string input)
+        {
+            var result = WhatTimeIsIt.GetMilitaryTimeFromStandardTime(input); 
+            
+            Assert.Equal(expected, result);
+        }
+    }
+}
