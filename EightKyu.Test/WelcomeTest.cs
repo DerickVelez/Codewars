bc934957dd0be8f0836d868ff92c0936f3be2651
Jan Derick Velez <jderickvelez@gmail.com>
2023-07-18 18:36:46 +0800

    Added Eight Kyu Kata: Welcome!

---
+++ EightKyu.Test/WelcomeTest.cs
@@ -0,0 +1,21 @@
+﻿using Eight.Kyuu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EightKyu.Test
+{
+    public class WelcomeTest
+    {
+        [Theory]
+        [InlineData("Welcome", "english")]
+
+       public void ShouldReturnTranslation(string expected, string output)
+        {
+            var result = Welcome.Greet(output);
+            Assert.Equal(expected, result);
+        }
+    }
+}
