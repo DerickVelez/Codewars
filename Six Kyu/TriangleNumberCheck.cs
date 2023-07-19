8497548d3428e50f2226034918b8eda0996707ec
Jan Derick Velez <jderickvelez@gmail.com>
2023-07-19 21:42:50 +0800

    Added Six Kyu Kata: Triangle Number Check

--- Six Kyu/TriangleNumberCheck.cs
+++ Six Kyu/TriangleNumberCheck.cs
@@ -6,7 +6,21 @@ using System.Threading.Tasks;
 
 namespace Six_Kyu
 {
-    internal class TriangleNumberCheck
+    public class TriangleNumberCheck
     {
+        public static bool isTriangleNumber(long number)
+        {
+
+
+            var n = Math.Sqrt(number / 0.5);
+            var reverseN = (int)Math.Floor(n);
+
+            int result = ((reverseN + 1) * reverseN) / 2;
+
+
+            if (result == number)
+            { return true; }
+            return false;
+        }
     }
 }
