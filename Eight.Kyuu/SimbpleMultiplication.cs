58e394a1e6a1f27e6b2a30d1b51d5d264da73123
Jan Derick Velez <jderickvelez@gmail.com>
2023-07-19 21:50:38 +0800

    Added Eight Kyu Kata: Simple Multiplication

---
+++ Eight.Kyuu/SimbpleMultiplication.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eight.Kyuu
+{
+    public class SimbpleMultiplication
+    {
+        public static int Multiply(int x) => x % 2 == 0 ? x * 8 : x * 9;
+    }
+}
