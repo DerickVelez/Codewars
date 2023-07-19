85dfe8beb393c00ca285da1b37fa88a694e1cab8
Jan Derick Velez <jderickvelez@gmail.com>
2023-07-19 21:34:58 +0800

    Added Eight Kyu Kata: Welcome

--- Eight.Kyuu/Welcome.cs
+++ Eight.Kyuu/Welcome.cs
@@ -6,7 +6,31 @@ using System.Threading.Tasks;
 
 namespace Eight.Kyuu
 {
-    internal class Welcome
+    public  class Welcome
     {
+        public static string Greet(string language)
+        {
+            var translation = new Dictionary<string, string>(){
+          {"english", "Welcome"},
+          {"czech", "Vitejte"},
+          {"danish", "Velkomst"},
+          {"dutch", "Welkom"},
+          {"estonian", "Tere tulemast"},
+          {"finnish", "Tervetuloa"},
+          {"flemish", "Welgekomen"},
+          {"french", "Bienvenue"},
+          {"german", "Willkommen"},
+          {"irish", "Failte"},
+          {"italian", "Benvenuto"},
+          {"latvian", "Gaidits"},
+          {"lithuanian", "Laukiamas"},
+          {"polish", "Witamy"},
+          {"spanish", "Bienvenido"},
+          {"swedish", "Valkommen"},
+          {"welsh", "Croeso"}
+            };
+            return translation.ContainsKey(language.ToLower()) ? translation[language.ToLower()] : "Welcome";
+
+        }
     }
 }
