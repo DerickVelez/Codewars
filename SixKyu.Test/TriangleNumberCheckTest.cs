6b2356ea7db092abba05bfb46cd428efa278c4bc
Jan Derick Velez <jderickvelez@gmail.com>
2023-07-19 21:41:29 +0800

    Added Six Kyu Kata: Triangle Number Check

---
+++ SixKyu.Test/TriangleNumberCheckTest.cs
@@ -0,0 +1,23 @@
+﻿using Six_Kyu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SixKyu.Test
+{
+    public class TriangleNumberCheckTest
+    {
+        [Theory]
+        [InlineData(true,312537501)]
+        [InlineData(true,61721605)]
+        [InlineData(true, 0)]
+
+        public void ShouldReturnTrueIfTriangle(bool expected, int triangleNumber)
+        {
+            var result = TriangleNumberCheck.isTriangleNumber(triangleNumber);
+           Assert.Equal(expected,result);
+        }
+    }
+}
