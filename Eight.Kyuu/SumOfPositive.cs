91315370af48ec1995b9b2feaf2e70e5b5e5dcb9
Jan Derick Velez <jderickvelez@gmail.com>
2023-07-20 20:38:24 +0800

    Added Eight Kyu Kata: Sum of Positive

---
+++ Eight.Kyuu/SumOfPositive.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eight.Kyuu
+{
+    public class SumOfPositive
+    {
+        public static int PositiveSum(int[] arr)
+        {
+            return arr.Where(n => n > 0).Sum();
+        }
+    }
+}
---
+++ EightKyu.Test/SumofPositiveTest.cs
@@ -0,0 +1,22 @@
+﻿using Eight.Kyuu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EightKyu.Test
+{
+    public class SumofPositiveTest
+    {
+        [Theory]
+        [InlineData(15 , new int[] { 1, 2, 3, 4, 5 })]
+        [InlineData(13, new int[] { 1, -2, 3, 4, 5 })]
+
+        public void ShouldReturnSumOfPositive(int expected, int[] input)
+        {
+            var result = SumOfPositive.PositiveSum(input);
+            Assert.Equal(expected, result);
+        }
+    }
+}
