f37da9a3684d24a67aa840c424a49ae959fcb10c
Jan Derick Velez <jderickvelez@gmail.com>
2023-07-21 14:26:24 +0800

    Added Eight Kyu Kata: Set Alarm

---
+++ Eight.Kyuu/SetAlarm.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eight.Kyuu
+{
+    public class SetAlarm
+    {
+        public static bool SetAlarms(bool employed, bool vacation) => employed == true && vacation == false ? true : false;
+    }
+}
---
+++ EightKyu.Test/SetAlarmTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics.SymbolStore;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Eight.Kyuu;
+
+namespace EightKyu.Test
+{
+    public class SetAlarmTest
+    {
+        [Theory]
+        [InlineData(true,true,false)]
+        [InlineData(false,false,false)]
+
+        public void ShouldReturnTrueIfOnVacation(bool expected, bool employed, bool vacation)
+        {
+            var result = SetAlarm.SetAlarms(employed, vacation);
+            Assert.Equal(expected,result);
+        }
+    }
+}
