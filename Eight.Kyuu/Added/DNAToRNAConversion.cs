287e804b4c7d8ae26cd26c5f277fbdde7fd375c3
Jan Derick Velez <jderickvelez@gmail.com>
2023-07-22 22:53:57 +0800

    Added Eight Kyu Kata: DNA to RNA Conversion

---
+++ Eight.Kyuu/Added/DNAToRNAConversion.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eight.Kyuu.Added
+{
+    public class DNAToRNAConversion
+    {
+        public string dnaToRna(string dna)
+        {
+            return dna.Replace("T", "U");
+        }
+    }
+}
---
+++ EightKyu.Test/DNAToRNAConversionTest.cs
@@ -0,0 +1,22 @@
+﻿using Eight.Kyuu.Added;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EightKyu.Test
+{
+    public class DNAToRNAConversionTest
+    {
+        [Theory]
+        [InlineData("UUUU", "TTTT")]
+        [InlineData("GACCGCCGCC", "GACCGCCGCC")]
+
+        public void ShouldReturnConvertedString(string expected, string input)
+        {
+            var result = DNAToRNAConversion.dnaToRna(input);
+            Assert.Equal(expected, result);
+        }
+    }
+}
