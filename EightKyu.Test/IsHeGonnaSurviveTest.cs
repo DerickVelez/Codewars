6902bb5ac8d28cc00c5ac809c798366c330ad944
Jan Derick Velez <jderickvelez@gmail.com>
2023-07-27 23:50:17 +0800

    Added Eight Kyu Kata: Is He Gonna Survive?

---
+++ EightKyu.Test/IsHeGonnaSurviveTest.cs
@@ -0,0 +1,22 @@
+﻿using Eight.Kyuu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EightKyu.Test
+{
+    public class IsHeGonnaSurviveTest
+    {
+        [Theory]
+        [InlineData(true, 100, 40)]
+        [InlineData(true, 10, 5)]
+
+        public void ShouldReturnTrueIfHeSurvive(bool expected, int bullets, int dragons)
+        {
+            var result = IsHeGonnaSurvive.Hero(bullets, dragons);
+            Assert.Equal(expected, result);
+        }
+    }
+}
