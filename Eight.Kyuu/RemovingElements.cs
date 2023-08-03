97e9ef12a49d4c5f98a44eaa135f41ef5c12f1fa
Jan Derick Velez <jderickvelez@gmail.com>
2023-08-03 10:49:16 +0800

    Added Eight Kyu Kata: Removing Elements

---
+++ Eight.Kyuu/RemovingElements.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eight.Kyuu
+{
+    public class RemovingElements
+    {
+          public static object[] RemoveEveryOther(object[] arr)
+        {
+          List<object> arrays = new List<object>();
+            
+           var flag = true;
+           foreach (object word in arr )
+          {
+             if (flag == true)
+            {
+               arrays.Add(word);
+            }
+             flag =!flag;
+           }
+          return arrays.ToArray();
+        }
+    }
+}
---
+++ EightKyu.Test/RemovingElementsTest.cs
@@ -0,0 +1,22 @@
+﻿using Eight.Kyuu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EightKyu.Test
+{
+    public class RemovingElementsTest
+    {
+        [Theory]
+        [InlineData(new object[] { "Hello", "Hello Again" }, new object[] { "Hello", "Goodbye", "Hello Again" })]
+        [InlineData(new object[] { 1, 3, 5, 7, 9 }, new object[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 })]
+
+        public void ShouldReturnFinalElements(object[] expected, object[] elements)
+        {
+            var result = RemovingElements.RemoveEveryOther(elements); 
+            Assert.Equal(expected, result);
+        }
+    }
+}
