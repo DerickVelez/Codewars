2949211de82dc478187e78742eae7fbad14a99d6
Jan Derick Velez <jderickvelez@gmail.com>
2023-08-04 11:06:51 +0800

    Added Seven Kyu Kata: Float Precision

---
+++ Seven.Kyu/FloatPrecision.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seven.Kyu
+{
+    public class FloatPrecision
+    {
+        public static double Round(double n) => Math.Round(n, 2);
+    }
+}
---
+++ SevenKyu.Test/FloatPrecisionTest.cs
@@ -0,0 +1,23 @@
+﻿using Seven.Kyu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SevenKyu.Test
+{
+    public class FloatPrecisionTest
+    {
+        [Theory]
+        [InlineData(1.14, 1.1356)]
+        [InlineData(2.35, 2.34545)]
+        [InlineData(445.23, 445.234234)]
+
+        public void ShouldReturnRoundedNum(float expected, float precision)
+        {
+            var result = FloatPrecision.Round(precision);
+            Assert.Equal(expected, result);
+        }
+    }
+}
