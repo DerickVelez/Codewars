ea07f8a89cf6ca380df9244a50f7de40f33b112b
Jan Derick Velez <jderickvelez@gmail.com>
2023-08-07 18:28:10 +0800

    Added Eight Kyu Kata: Short Long Short

---
+++ Eight.Kyuu/ShortLongShort.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eight.Kyuu
+{
+    public class ShortLongShort
+    {
+        public static string Solution(string a, string b) => a.Length < b.Length ? a + b + a : b + a + b;
+
+    }
+}
---
+++ EightKyu.Test/ShortLongShortTest.cs
@@ -0,0 +1,24 @@
+﻿using Eight.Kyuu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EightKyu.Test
+{
+    public class ShortLongShortTest
+    {
+        [Fact]
+
+        public void ShouldReturnShortLongShortSequence()
+        {
+            var expected = "abba";
+            var actualA = "a";
+            var actualB = "bb";
+
+            var result = ShortLongShort.Solution(actualA, actualB);
+            Assert.Equal(expected, result);
+        }
+    }
+}
