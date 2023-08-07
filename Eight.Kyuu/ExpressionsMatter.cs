fa57023fed832805f4e003aadb92fc98d2cd341e
Jan Derick Velez <jderickvelez@gmail.com>
2023-08-07 19:52:50 +0800

    Added Eight Kyu Kata: Expressions Matter

---
+++ Eight.Kyuu/ExpressionsMatter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eight.Kyuu
+{
+    public class ExpressionsMatter
+    {
+        public   static int ExpressionsMatterExecution(int a, int b, int c)
+        {
+            int num1 = a * (b + c);
+            int num2 = a * b * c;
+            int num3 = a + b * c;
+            int num4 = (a + b) * c;
+            int num5 = a + b + c;
+
+            int num6 = Math.Max(num1, num2);
+            int num7 = Math.Max(num3, num4);
+            int num8 = Math.Max(num6, num7);
+
+            return Math.Max(num5, num8);
+        }
+    }
+}
---
+++ EightKyu.Test/ExpressionsMatterTest.cs
@@ -0,0 +1,21 @@
+﻿using Eight.Kyuu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EightKyu.Test
+{
+    public class ExpressionsMatterTest
+    {
+        [Theory]
+        [InlineData(9,1,2,3)]
+
+        public void ShouldReturnMax(int expected, int a, int b, int c)
+        {
+            var result = ExpressionsMatter.ExpressionsMatterExecution(a, b, c);
+            Assert.Equal(expected, result);
+        }
+    }
+}
