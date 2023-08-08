7dcffcb550b2e8742a0914691d2a97e86cdc0004
Jan Derick Velez <jderickvelez@gmail.com>
2023-08-08 22:59:28 +0800

    Added Eight Kyu Kata: Jenny's Secret Message

---
+++ Eight.Kyuu/JennySecretMessage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eight.Kyuu
+{
+    public class JennySecretMessage
+    {
+    
+    public static string greet(string name) => name == "Johnny" ? "Hello, my love!" : "Hello, " + name + "!";
+}
+}
---
+++ EightKyu.Test/JennySecretMessageTest.cs
@@ -0,0 +1,22 @@
+﻿using Eight.Kyuu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EightKyu.Test
+{
+    public class JennySecretMessageTest
+    {
+        [Theory]
+        [InlineData("Hello, Jane!", "Jane")]
+        [InlineData("Hello, my love!", "Johnny")]
+
+        public void ShouldReturnSecretMessage(string expected, string actual)
+        {
+            var result = JennySecretMessage.greet(actual); 
+            Assert.Equal(expected, result);
+        }
+    }
+}
