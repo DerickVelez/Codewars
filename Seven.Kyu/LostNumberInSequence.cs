4c45b320ba5d46fe47011fdcf5387fe8a2417469
Jan Derick Velez <jderickvelez@gmail.com>
2023-08-09 23:06:05 +0800

    Added Seven Kyu Kata: Lost Number In Sequence

---
+++ Seven.Kyu/LostNumberInSequence.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seven.Kyu
+{
+    public class LostNumberInSequence
+    {
+        public static int FindDeletedNumber(int[] AstartingList, int[] BmixedList)
+        {
+            var startingList = AstartingList.ToList();
+            var mixedList = BmixedList.ToList();
+            var missingnumber = startingList.Except(mixedList);
+
+            int numbers = 0;
+            foreach (int number in missingnumber)
+            { numbers = +number; }
+
+            return numbers;
+
+        }
+    }
+}
---
+++ SevenKyu.Test/LostNumberInSequenceTest.cs
@@ -0,0 +1,21 @@
+﻿using Seven.Kyu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SevenKyu.Test
+{
+    public class LostNumberInSequenceTest
+    {
+        [Theory]
+        [InlineData(6, new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 }, new int[] { 5, 7, 1, 9, 4, 8, 2, 3 })]
+        public void ShouldReturnMissingNum(int expected,int[] Alist, int[] Blist)
+        {
+            var result = LostNumberInSequence.FindDeletedNumber(Alist, Blist);
+            Assert.Equal(expected, result);
+        }
+
+    }
+}
