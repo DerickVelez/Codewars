4dab1650a600967ff847cc546a5c823cc0d5e570
Jan Derick Velez <jderickvelez@gmail.com>
2023-08-15 22:45:16 +0800

    Added Eight Kyu Kata: Welcome to the City

---
+++ Eight.Kyuu/WelcomeToTheCity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eight.Kyuu
+{
+    public class WelcomeToTheCity
+    {
+        public static string SayHello(string[] name, string city, string state)
+        {
+            return ($"Hello, {string.Join(" ", name)}! Welcome to {city}, {state}!");
+        }
+    }
+}
---
+++ EightKyu.Test/WelcomeToTheCityTest.cs
@@ -0,0 +1,22 @@
+﻿using Eight.Kyuu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EightKyu.Test
+{
+    public class WelcomeToTheCityTest
+    {
+        [Theory]
+        [InlineData("Hello, John Smith! Welcome to Phoenix, Arizona!", new string[] { "John", "Smith" }, "Phoenix", "Arizona")]
+        [InlineData("Hello, Franklin Delano Roosevelt! Welcome to Chicago, Illinois!", new string[] { "Franklin", "Delano", "Roosevelt" },"Chicago", "Illinois")]
+
+        public void ShouldReturnConvertedString(string expected, string[] a, string b, string c)
+        {
+            var result = WelcomeToTheCity.SayHello(a, b, c);
+            Assert.Equal(expected, result);
+        }
+    }
+}
