7299e24b3bce5779f7a0e08e630e71aae8d5bc35
Jan Derick Velez <jderickvelez@gmail.com>
2023-08-17 22:19:34 +0800

    Added Seven Kyu Kata: Jaden Casing Stirng

---
+++ Seven.Kyu/JasonCasingString.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seven.Kyu
+{
+    public static class JasonCasingString
+    {
+            public static string ToJadenCase(this string phrase)
+            {
+
+                TextInfo textInfo = new CultureInfo("en-US", false).TextInfo;
+                return textInfo.ToTitleCase(phrase);
+            }
+        
+    }
+}
