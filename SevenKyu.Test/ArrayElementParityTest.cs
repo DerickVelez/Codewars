fe78dd17360abf7452657641e54e74cc1e74364e
Jan Derick Velez <jderickvelez@gmail.com>
2023-08-18 20:50:31 +0800

    Added Seven Kyu Kata: Array Element Parity

---
+++ SevenKyu.Test/ArrayElementParityTest.cs
@@ -0,0 +1,22 @@
+﻿using Seven.Kyu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SevenKyu.Test
+{
+    public class ArrayElementParityTest
+    {
+        [Theory]
+        [InlineData(3, new int[] { 1, -1, 2, -2, 3 })]
+        [InlineData(-38, new int[] { -110, 110, -38, -38, -62, 62, -38, -38, -38 })]
+    
+        public void ShouldReturnUniqueInt(int expected, int[] actual)
+        {
+            var result = ArrayElementParity.Solve(actual);
+            Assert.Equal(expected, result);
+        }
+    }
+}
