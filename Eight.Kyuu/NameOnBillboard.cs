784b73e5db2084ceb4e67c1c20250ce7f5acffda
Jan Derick Velez <jderickvelez@gmail.com>
2023-08-18 20:40:01 +0800

    Added Eight Kyu Kata: Name on Billboard

---
+++ Eight.Kyuu/NameOnBillboard.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eight.Kyuu
+{
+    public class NameOnBillboard
+    {
+        public static double Billboard(string name, double price = 30)
+        {
+
+            double adPrice = 0;
+
+            for (var i = 0; i < name.Length; i++)
+            {
+                adPrice = adPrice += price;
+            }
+            return adPrice;
+        }
+    }
+}
---
+++ EightKyu.Test/NameOnBillboardTest.cs
@@ -0,0 +1,25 @@
+﻿using Eight.Kyuu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EightKyu.Test
+{
+    public class NameOnBillboardTest
+    {
+        [Fact]
+        
+        public void ShouldReturnName()
+        {
+            var Actual = "Jeong-Ho Aristotelis";
+            var expected = 600;
+
+            var result = NameOnBillboard.Billboard(Actual);
+
+            Assert.Equal(expected, result);
+
+        }
+    }
+}
