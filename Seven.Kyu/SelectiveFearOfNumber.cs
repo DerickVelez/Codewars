14d7f678023d8fab1d071d2f84aeef993d72d7e3
Jan Derick Velez <jderickvelez@gmail.com>
2023-08-22 23:14:12 +0800

    Added Seven Kyu Kata: Selective Fear Of Numbers

---
+++ Seven.Kyu/SelectiveFearOfNumber.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seven.Kyu
+{
+    public class SelectiveFearOfNumber
+    {
+        public static bool AmIAfraid(string day, int num)
+        {
+            if (day == "Monday" && num == 12)
+                return true;
+            if (day == "Wednesday" && num == 34)
+                return true;
+            if (day == "Saturday" && num == 56)
+                return true;
+            if (day == "Sunday" && num == 666 || num == -666)
+                return true;
+            if (day == "Thursday" && num == 0)
+                return true;
+            if (day == "Friday" && num % 2 == 0)
+                return true;
+            if (day == "Tuesday" && num > 95)
+                return true;
+            else
+                return false;
+
+        }
+    }
+}
---
+++ SevenKyu.Test/SelectiveFearOfNumberTest.cs
@@ -0,0 +1,22 @@
+﻿using Seven.Kyu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SevenKyu.Test
+{
+    public class SelectiveFearOfNumberTest
+    {
+        [Theory]
+        [InlineData(true, "Monday", 12)]
+        [InlineData(true,"Tuesday", 100)]
+
+        public void ShouldReturnTrueIfInRecord(bool expected, string day,  int count)
+        {
+            var result = SelectiveFearOfNumber.AmIAfraid(day, count);
+            Assert.Equal(expected, result);
+        }
+    }
+}
