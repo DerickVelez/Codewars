bb25b41b108b269c0dc8a9cb467e1147e706b72d
Jan Derick Velez <jderickvelez@gmail.com>
2023-08-23 09:51:37 +0800

    Added Six Kyu Kata: Handshake Problem

---
+++ Six Kyu/HandshakeProblem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Six_Kyu
+{
+    public class HandshakeProblem
+    {
+        public static int GetParticipants(int handshakes) => handshakes == 0 ? 0 : (int)Math.Round(Math.Sqrt(handshakes * 2)) + 1;
+    }
+}
---
+++ SixKyu.Test/HandShakeProblemTest.cs
@@ -0,0 +1,25 @@
+﻿using Six_Kyu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SixKyu.Test
+{
+    public class HandShakeProblemTest
+    {
+        [Theory]
+        [InlineData(0,0)]
+        [InlineData(2,1)]
+        [InlineData(3,3)]
+        [InlineData(4,6)]
+        [InlineData(5,7)]
+
+        public void ShouldReturnNumberofPeople(int expected, int handshake)
+        {
+            var result = HandshakeProblem.GetParticipants(handshake);
+            Assert.Equal(expected, result);
+        }
+    }
+}
