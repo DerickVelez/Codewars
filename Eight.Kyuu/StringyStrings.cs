134910114b4082072f4430cac9766abe18554622
Jan Derick Velez <jderickvelez@gmail.com>
2023-08-24 08:35:18 +0800

    Added Eight Kyu Kata: Stringy Strings

---
+++ Eight.Kyuu/StringyStrings.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eight.Kyuu
+{
+    public class StringyStrings
+    {
+        public static string Stringy(int size)
+        {
+            List<string> result = new List<string>();
+            var flag = true;
+            for (var i = 0; i < size; i++)
+            {
+                if (flag == true)
+                {
+                    result.Add("1");
+                }
+                else
+                { result.Add("0"); }
+
+                flag = !flag;
+            }
+
+            return string.Join("", result);
+        }
+    }
+}
---
+++ EightKyu.Test/StringyStringTest.cs
@@ -0,0 +1,24 @@
+﻿using Eight.Kyuu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EightKyu.Test
+{
+    public class StringyStringTest
+    {
+        [Theory]
+        [InlineData("1010101010",10)]
+        [InlineData("10",2)]
+        [InlineData("",0)]
+
+        public void ShouldReturnConvertedBinary(string expected, int input)
+        {
+            var result = StringyStrings.Stringy(input);
+            Assert.Equal(expected, result);
+        }
+
+    }
+}
