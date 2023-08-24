8ee1284c1a7c98aa498939684151edb308ff2e3f
Jan Derick Velez <jderickvelez@gmail.com>
2023-08-24 09:30:37 +0800

    Added Seven Kyu Kata: Breaking Chocolate Problem

---
+++ Seven.Kyu/BreakingChocolateProblem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seven.Kyu
+{
+    public class BreakingChocolateProblem
+    {
+        public static int BreakChocolate(int n, int m) => n == 0 || m == 0 ? 0 : n * m - 1;
+    }
+}
---
+++ SevenKyu.Test/BreakingChocolateProblemTest.cs
@@ -0,0 +1,25 @@
+﻿using Seven.Kyu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SevenKyu.Test
+{
+    public class BreakingChocolateProblemTest
+    {
+        [Theory]
+        [InlineData(24,5,5)]
+        [InlineData(27,7,4)]
+        [InlineData(0,1,1)]
+        [InlineData(0,0,0)]
+        [InlineData(5,6,1)]
+
+        public void ShouldReturnNumberOfBreaks(int expected, int n, int m)
+        {
+            var result = BreakingChocolateProblem.BreakChocolate(n, m);
+            Assert.Equal(expected, result);
+        }
+    }
+}
