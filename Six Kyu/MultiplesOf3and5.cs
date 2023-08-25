ae25221d0efae0cfac0cf72eeb7a53055c0ce0ba
Jan Derick Velez <jderickvelez@gmail.com>
2023-08-25 22:58:30 +0800

    Added Six Kyu Kata: Multiples of 3 and 5

---
+++ Six Kyu/MultiplesOf3and5.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Six_Kyu
+{
+    public class MultiplesOf3and5
+    {
+        public static int Solution(int value)
+        {
+            var multiples = new List<int> { };
+
+            for (var i = 0; i < value; i += 3)
+            {
+                multiples.Add(i);
+            }
+            for (var i = 0; i < value; i += 5)
+            {
+                multiples.Add(i);
+            }
+
+            return multiples.Distinct().Sum();
+        }
+    }
+}
---
+++ SixKyu.Test/MutliplesOf3and5Test.cs
@@ -0,0 +1,25 @@
+﻿using Six_Kyu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SixKyu.Test
+{
+    public class MutliplesOf3and5Test
+    {
+        [Theory]
+        [InlineData(23,10)]
+        [InlineData(78,20)]
+        [InlineData(9168,200)]
+        [InlineData(0,0)]
+
+        public void ShouldReturnMultiplesSum(int expected, int input)
+
+        {
+            var result = MultiplesOf3and5.Solution(input);
+            Assert.Equal(expected, result);
+        }
+    }
+}
