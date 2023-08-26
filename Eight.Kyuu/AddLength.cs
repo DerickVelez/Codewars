fb3fdba17daa716f95a0127dcea47162357c1265
Jan Derick Velez <jderickvelez@gmail.com>
2023-08-26 23:26:53 +0800

    Added Eight Kyu Kata: Add Length

---
+++ Eight.Kyuu/AddLength.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eight.Kyuu
+{
+    public class AddLength
+    {
+        public static string[] AddLengthInString(string str)
+        {
+            var words = str.Split();
+            var result = new List<string>();
+
+            foreach (string subword in words)
+            {
+                result.Add($"{subword} {subword.Length.ToString()}");
+            }
+            return result.ToArray();
+        }
+    }
+}
---
+++ EightKyu.Test/AddLenghtTest.cs
@@ -0,0 +1,22 @@
+﻿using Eight.Kyuu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EightKyu.Test
+{
+    public class AddLenghtTest
+    {
+        [Theory]
+        [InlineData(new string[] { "apple 5", "ban 3" }, "apple ban")]
+        [InlineData(new string[] { "you 3", "will 4", "win 3" }, "you will win")]
+
+        public void ShouldReturnStringWithLength(string[] expected, string input)
+        {
+            var result = AddLength.AddLengthInString(input);
+            Assert.Equal(expected, result);
+        }
+    }
+}
