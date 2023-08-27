fb826aaeb7d1a5e5a59b899d2cdec0fe2f24305c
Jan Derick Velez <jderickvelez@gmail.com>
2023-08-27 23:50:23 +0800

    Added Six Kyu Kata: Who Likes It?

---
+++ Six Kyu/WhoLikesIt.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Six_Kyu
+{
+    public class WhoLikesIt
+    {
+        public static string Likes(string[] name)
+        {
+            var count = name.Length;
+
+            if (count == 0)
+            { return "no one likes this"; }
+            if (count == 1)
+            { return $"{name[0]} likes this"; }
+            if (count == 2)
+            { return $"{name[0]} and {name[1]} like this"; }
+            if (count == 3)
+            { return $"{name[0]}, {name[1]} and {name[2]} like this"; }
+
+            else
+                return $"{name[0]}, {name[1]} and {count - 2} others like this";
+        }
+    }
+}
---
+++ SixKyu.Test/WhoLikesItTest.cs
@@ -0,0 +1,25 @@
+﻿using Six_Kyu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SixKyu.Test
+{
+    public class WhoLikesItTest
+    {
+        [Theory]
+        [InlineData("no one likes this", new string[0])]
+        [InlineData("Peter likes this", new string[] { "Peter" })]
+        [InlineData("Jacob and Alex like this",new string[] {"Jacob", "Alex"})]
+        [InlineData("Max, John and Mark like this", new string[] { "Max", "John", "Mark" })]
+        [InlineData("Alex, Jacob and 2 others like this", new string[] { "Alex", "Jacob", "Mark", "Max" })]
+ 
+        public void ShouldReturnString(string expected, string[] actual)
+        {
+            var result = WhoLikesIt.Likes(actual); 
+            Assert.Equal(expected, result);
+        }
+    }
+}
