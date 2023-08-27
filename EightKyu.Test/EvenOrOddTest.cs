0c75171f8d85af4199667c72e615e068ef75b212
Jan Derick Velez <jderickvelez@gmail.com>
2023-08-27 23:59:48 +0800

    Added Eight Kyu Kata: Even  Or Odd

---
+++ EightKyu.Test/EvenOrOddTest.cs
@@ -0,0 +1,23 @@
+﻿using Eight.Kyuu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EightKyu.Test
+{
+    public class EvenOrOddTest
+    {
+        [Theory]
+        [InlineData("Even",2)]
+        [InlineData("Odd",3)]
+        [InlineData("Odd", -1)]
+
+        public void ShouldReturnIfEvenOrOdd(string expected, int input)
+        {
+            var result = EvenOrOdd.EvenOrOddmethod(input);
+            Assert.Equal(expected, result);
+        }
+    }
+}
