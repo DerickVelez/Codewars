484c75af2dadf4e8de061a05bb9f20853d70f7a2
Jan Derick Velez <jderickvelez@gmail.com>
2023-08-27 23:56:38 +0800

    Added Eight Kyu Kata: Even  Or Odd

---
+++ Eight.Kyuu/EvenOrOdd.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eight.Kyuu
+{
+    public class EvenOrOdd
+    {
+        public static string EvenOrOddmethod(int number) => number % 2 == 0 ? "Even" : "Odd";
+
+    }
+}
