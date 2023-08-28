4c498c8244d410bfaf7becc9efdd0a3c586caa17
Jan Derick Velez <jderickvelez@gmail.com>
2023-08-28 23:08:08 +0800

    Added Five Kyu Kata: RGB to Hex Conversion

---
+++ Five Kyu Kata Test/RGBtoHexConversionTest.cs
@@ -0,0 +1,23 @@
+﻿using Five_Kyu_Kata;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Five_Kyu_Kata_Test
+{
+    public class RGBtoHexConversionTest
+    {
+        [Theory]
+        [InlineData("FFFFFF", 255, 255, 255)]
+        [InlineData("FFFFFF", 255, 255, 300)]
+        [InlineData("9400D3", 148, -20, 211)]
+
+        public void ShouldReturnHexConversion(string expected, int r, int g, int b)
+        {
+            var result = RGBtoHexConversion.Rgb(r,g, b);
+            Assert.Equal(expected, result);
+        }
+    }
+}
---
+++ Five Kyu Kata/RGBtoHexConversion.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Drawing;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Five_Kyu_Kata
+{
+    public class RGBtoHexConversion
+    {
+        public static string Rgb(int r, int g, int b)
+        {
+            var red = r > 255 ? 255 : (r < 0 ? 0 : r);
+            var green = g > 255 ? 255 : (g < 0 ? 0 : g);
+            var blue = b > 255 ? 255 : (b < 0 ? 0 : b);
+
+            return red.ToString("X2") + green.ToString("X2") + blue.ToString("X2");
+        }
+    }
+}
