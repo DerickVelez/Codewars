0ef749328a27194ffa88f2cbd692bb997d68e641
Jan Derick Velez <jderickvelez@gmail.com>
2023-08-29 18:05:47 +0800

    Added Eight Kyu Kata: Fake Binary

---
+++ Eight.Kyuu/FakeBinary.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eight.Kyuu
+{
+    public class FakeBinary
+    {
+        public static string FakeBin(string x)
+        {
+
+            var result = new List<string>();
+
+            foreach (int num in x)
+            {
+                if (num < '5')
+                { result.Add("0"); }
+                else
+                { result.Add("1"); }
+            }
+
+            return string.Join("", result);
+
+        }
+    }
+}
---
+++ EightKyu.Test/FakeBinaryTest.cs
@@ -0,0 +1,23 @@
+﻿using Eight.Kyuu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EightKyu.Test
+{
+    public class FakeBinaryTest
+    {
+        [Theory]
+        [InlineData("01011110001100111", "01011110001100111")]
+        [InlineData("011011110000101010000011011", "366058562030849490134388085")]
+        [InlineData("101000111101101", "509321967506747")]
+
+        public void ShouldReturnFakeBinary(string expected, string input)
+        {
+            var result = FakeBinary.FakeBin(input);
+            Assert.Equal(expected, result);
+        }
+    }
+}
