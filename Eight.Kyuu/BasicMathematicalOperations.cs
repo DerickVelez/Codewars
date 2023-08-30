ac07f7dde52db9898b2e92bdd818c49cdf9b02bb
Jan Derick Velez <jderickvelez@gmail.com>
2023-08-30 09:06:42 +0800

    Added Eight Kyu Kata: Basic Mathematical Operation

---
+++ Eight.Kyuu/BasicMathematicalOperations.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eight.Kyuu
+{
+    public class BasicMathematicalOperations
+    {
+        public static double basicOp(char operation, double value1, double value2)
+        {
+            switch (operation)
+            {
+                case '+': return value1 + value2;
+                case '-': return value1 - value2;
+                case '*': return value1 * value2;
+                case '/': return value1 / value2;
+            }
+            return 0;
+        }
+    }
+}
---
+++ EightKyu.Test/BasicMathematicalOperationsTest.cs
@@ -0,0 +1,23 @@
+﻿using Eight.Kyuu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EightKyu.Test
+{
+    public class BasicMathematicalOperationsTest
+    {
+        [Theory]
+        [InlineData(11, '+', 4, 7)]
+        [InlineData(-3, '-', 15, 18)]
+        [InlineData(25, '*', 5, 5)]
+
+        public void ShouldReturnResult(int expected, char operation,int input1,int input2)
+        {
+            var result = BasicMathematicalOperations.basicOp(operation, input1, input2);
+            Assert.Equal(expected, result);
+        }
+    }
+}
