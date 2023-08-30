da3071887a70380e33e9ce41600785cb79e2df83
Jan Derick Velez <jderickvelez@gmail.com>
2023-08-30 21:06:36 +0800

    Added Eight Kyu Kata: Gravity Flip

---
+++ Eight.Kyuu/GravityFlip.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eight.Kyuu
+{
+    public class GravityFlip
+    {
+        public static int[] Flip(char dir, int[] arr)
+        {
+
+            Array.Sort(arr);
+
+            if (dir == 'L')
+            { Array.Reverse(arr); }
+
+            return arr;
+        }
+    }
+}
---
+++ EightKyu.Test/GravityFlipTest.cs
@@ -0,0 +1,23 @@
+﻿using Eight.Kyuu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EightKyu.Test
+{
+    public class GravityFlipTest
+    {
+        [Theory]
+        [InlineData(new int[] { 1, 4, 5, 6, 7 }, 'R', new int[] { 4, 5, 6, 7, 1 })]
+        [InlineData(new int[] { 2, 4, 7, 13, 93 }, 'R', new int[] { 13, 2, 4, 7, 93 })]
+        [InlineData(new int[] { 12, 1, 0, 0, 0 },'L', new int[] { 12, 1, 0, 0, 0 })]
+
+        public void ShouldReturnSortedArray(int[] expected, char dir, int[] arr)
+        {
+            var result = GravityFlip.Flip(dir, arr);
+            Assert.Equal(expected, result);
+        }
+    }
+}
