c1fcf38d13d56739ccee20ce044e8fbcfd50aa5b
Jan Derick Velez <jderickvelez@gmail.com>
2023-08-31 12:29:38 +0800

    Added Eight Kyu Kata: Bin to Decimal

---
+++ Eight.Kyuu/BintoDecimal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eight.Kyuu
+{
+    public class BintoDecimal
+    {
+        public static int binToDec(string s)
+        {
+            return Convert.ToInt32(s, 2);
+        }
+    }
+}
---
+++ EightKyu.Test/BinToDecimalTest.cs
@@ -0,0 +1,24 @@
+﻿using Eight.Kyuu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EightKyu.Test
+{
+    public class BinToDecimalTest
+    {
+        [Theory]
+        [InlineData("0",0)]
+        [InlineData("1",1)]
+        [InlineData("2",10)]
+
+        public void ShouldReturnBinaryEquivalent(string value, int expected)
+
+        {
+            var result = BintoDecimal.binToDec(value);
+            Assert.Equal(expected, result);
+        }
+    }
+}
