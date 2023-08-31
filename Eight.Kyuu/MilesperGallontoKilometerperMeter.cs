1e4590e71c7e246907e88622fb7d9a15f374e227
Jan Derick Velez <jderickvelez@gmail.com>
2023-08-31 11:55:45 +0800

    Added Eight Kyu Kata: Miles per Gallon to Kilometer per Meter

---
+++ Eight.Kyuu/MilesperGallontoKilometerperMeter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eight.Kyuu
+{
+    public class MilesperGallontoKilometerperMeter
+    {
+        public static double Converter(int mpg)
+        {
+            return Math.Round(((mpg / 4.54609188) * 1.609344), 2);
+        }
+    }
+}
---
+++ EightKyu.Test/MilesperGallontoKilometerperMeterTest.cs
@@ -0,0 +1,25 @@
+﻿using Eight.Kyuu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EightKyu.Test
+{
+    public class MilesperGallontoKilometerperMeterTest
+    {
+        [Theory]
+        [InlineData(3.54,10)]
+        [InlineData(7.08,20)]
+        [InlineData(8.50,24)]
+
+        public void ShouldReturnResult(double expected, int count)
+        {
+            var result = MilesperGallontoKilometerperMeter.Converter(count);
+            Assert.Equal(expected, result);
+        }
+
+
+    }
+}
