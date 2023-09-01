f382b886cd30b91d201319aaf942f493cf0d6f49
Jan Derick Velez <jderickvelez@gmail.com>
2023-09-01 11:09:01 +0800

    Added Seven Kyu Kata: Return Subtring Instance Count

---
+++ Seven.Kyu/ReturnSubstringInstanceCount.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Seven.Kyu
+{
+    public class ReturnSubstringInstanceCount
+    {
+        public static int SubstringCount(string fullText, string searchText)
+        {
+            return new Regex(Regex.Escape(searchText)).Matches(fullText).Count;
+        }
+    }
+}
---
+++ SevenKyu.Test/RetutnSubstringInstanceCountTest.cs
@@ -0,0 +1,22 @@
+﻿using Seven.Kyu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SevenKyu.Test
+{
+    public class RetutnSubstringInstanceCountTest
+    {
+        [Theory]
+        [InlineData(2,"abcdeb", "b" )]
+        [InlineData(3,"ccddeeccddeecc", "cc")]
+
+        public void ShouldReturnOccurences(int expected, string fulltext, string searchtext)
+        {
+            var result = ReturnSubstringInstanceCount.SubstringCount(fulltext, searchtext);
+            Assert.Equal(expected, result);
+        }
+    }
+}
