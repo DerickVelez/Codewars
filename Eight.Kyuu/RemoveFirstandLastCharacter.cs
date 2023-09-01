0cd848f5525aaf2722dae33f820fc747d9661b94
Jan Derick Velez <jderickvelez@gmail.com>
2023-09-01 11:17:07 +0800

    Added Eight Kyu Kata: Remove First and Last Character

---
+++ Eight.Kyuu/RemoveFirstandLastCharacter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eight.Kyuu
+{
+    public class RemoveFirstandLastCharacter
+    {
+        public static string Remove_char(string s)
+        {
+            return s.Substring(1).Substring(0, s.Length - 2);
+        }
+    }
+}
---
+++ EightKyu.Test/RemoveFirstandLastCharacterTest.cs
@@ -0,0 +1,23 @@
+﻿using Eight.Kyuu;
+using Eight.Kyuu.Added;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EightKyu.Test
+{
+    public class RemoveFirstandLastCharacterTest
+    {
+        [Theory]
+        [InlineData("loquen", "eloquent")]
+        [InlineData("eric","Derick")]
+
+        public void ShouldReturnTrimmedString(string expected, string input)
+        {
+            var result = RemoveFirstandLastCharacter.Remove_char(input);
+            Assert.Equal(expected, result);
+        }
+    }
+}
