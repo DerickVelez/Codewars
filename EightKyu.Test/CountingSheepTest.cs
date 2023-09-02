b9209b3bde9eb56d17fc7de8cefe26cf5a574f0f
Jan Derick Velez <jderickvelez@gmail.com>
2023-09-02 23:13:36 +0800

    Added Eight Kyu Kata: Counting Sheep

---
+++ EightKyu.Test/CountingSheepTest.cs
@@ -0,0 +1,22 @@
+﻿using Eight.Kyuu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EightKyu.Test
+{
+    public class CountingSheepTest
+    {
+        [Theory]
+        [InlineData(2, new bool[] { true, false, true, true, false, true, true, false, true })]
+        [InlineData(6, new bool[] { true, true, true, true, true, true, true, true, true })]
+
+        public void ShouldReturnNumberofSheep(int expected, bool[] actual)
+        {
+            var result = CountingSheep.CountSheeps(actual); 
+            Assert.Equal(expected, result);
+        }
+    }
+}
