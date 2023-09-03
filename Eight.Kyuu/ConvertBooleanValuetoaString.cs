d9eeb396a54de05e8eb8fda3d5febfbdc5697ee6
Jan Derick Velez <jderickvelez@gmail.com>
2023-09-03 08:42:18 +0800

    Added Eight Kyu Kata: Convert Boolean to a String

---
+++ Eight.Kyuu/ConvertBooleanValuetoaString.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eight.Kyuu
+{
+    public class ConvertBooleanValuetoaString
+    {
+        public static string boolToWord(bool word) => word == true ? "Yes" : "No";
+    }
+}
---
+++ EightKyu.Test/ConvertBooleanValueToaStringTest.cs
@@ -0,0 +1,22 @@
+﻿using Eight.Kyuu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EightKyu.Test
+{
+    public class ConvertBooleanValueToaStringTest
+    {
+        [Theory]
+        [InlineData("Yes", true)]
+        [InlineData("No", false)]
+
+        public void ShouldReturnConvertedValue(string expected,bool value)
+        {
+            var result = ConvertBooleanValuetoaString.boolToWord(value);
+            Assert.Equal(expected, result);
+        }
+    }
+}
