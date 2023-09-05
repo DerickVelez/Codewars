6122969d979aa51e4dc1f5a9c69a86f28b80bc3b
Jan Derick Velez <jderickvelez@gmail.com>
2023-09-05 21:45:47 +0800

    Added Eight Kyu Kata: What is Between

---
+++ Eight.Kyuu/WhatisBetween.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eight.Kyuu
+{
+    public class WhatisBetween
+    {
+        public static int[] Between(int a, int b)
+        {
+
+            var list = new List<int>();
+
+            while (a <= b)
+            {
+                list.Add(a);
+                a++;
+            }
+            return list.ToArray();
+        }
+    }
+}
---
+++ EightKyu.Test/WhatisBetweenTest.cs
@@ -0,0 +1,23 @@
+﻿using Eight.Kyuu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EightKyu.Test
+{
+    public class WhatisBetweenTest
+    {
+ 
+        [Theory]
+        [InlineData(new int[] { 0, 1, 2, 3 },0,3)]
+        [InlineData(new int[] { -2, -1, 0, 1, 2 }, -2, 2)]
+
+        public void ShouldReturnBetweenNumbers(int[] expected, int a, int b)
+        {
+            var result = WhatisBetween.Between(a, b);
+            Assert.Equal(expected, result);
+        }
+    }
+}
