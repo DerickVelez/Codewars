db8058e08b28595d611a78aa1ca86268edb4e9c3
Jan Derick Velez <jderickvelez@gmail.com>
2023-09-05 22:37:37 +0800

    Added Eight Kyu Kata: FIXME: Replace all dots

---
+++ Eight.Kyuu/FIXME Replacealldots.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Eight.Kyuu
+{
+    public class FIXME_Replacealldots
+    {
+        public static string ReplaceDots(string str)
+        {
+            return Regex.Replace(str, @"\.", "-");
+        }
+    }
+}
---
+++ EightKyu.Test/IXME_ReplacealldotsTest.cs
@@ -0,0 +1,23 @@
+﻿using Eight.Kyuu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EightKyu.Test
+{
+    public class IXME_ReplacealldotsTest
+    {
+        [Theory]
+        [InlineData("one-two-three", "one.two.three")]
+        [InlineData("no dots", "no dots")]
+        [InlineData("", "")]
+
+        public void ShouldReturnNoDots(string input, string output)
+        {
+            var result = FIXME_Replacealldots.ReplaceDots(output); 
+            Assert.Equal(input, result);
+        }
+    }
+}
