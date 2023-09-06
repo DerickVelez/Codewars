30b4f727439e2a8460aa070fc9656bb2540f9308
Jan Derick Velez <jderickvelez@gmail.com>
2023-09-06 23:00:54 +0800

    Added Eight Kyu Kata: Get Ascii Value of Character

---
+++ Eight.Kyuu/GetAsciiValueofCharacter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eight.Kyuu
+{
+    public class GetAsciiValueofCharacter
+    {
+        public static int GetASCII(char c)
+        {
+            return (int)c;
+        }
+    }
+}
---
+++ EightKyu.Test/GetAsciiValueofCharacterTest.cs
@@ -0,0 +1,22 @@
+﻿using Eight.Kyuu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EightKyu.Test
+{
+    public class GetAsciiValueofCharacterTest
+    {
+        [Theory]
+        [InlineData(65,'A')]
+        [InlineData(32,' ')]
+
+        public void ShouldReturnASCIIValue(int expected, char value)
+        {
+            var result = GetAsciiValueofCharacter.GetASCII(value);
+            Assert.Equal(expected, result);
+        }
+    }
+}
