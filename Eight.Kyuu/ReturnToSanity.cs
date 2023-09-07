7e63c1e23c39189c7db5d84b4dcf34bdf57966a1
Jan Derick Velez <jderickvelez@gmail.com>
2023-09-07 11:56:50 +0800

    Added Eight Kyu Kata: Retur to Sanity

---
+++ Eight.Kyuu/ReturnToSanity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eight.Kyuu
+{
+    public class ReturnToSanity
+    {
+        public static Dictionary<string, string> Mystery()
+        {
+            Dictionary<string,
+          string> results = new
+            Dictionary<string,
+          string>();
+            results.Add("sanity", "hello");
+            return results;
+        }
+    }
+}
---
+++ EightKyu.Test/ReturnToSanityTest.cs
@@ -0,0 +1,22 @@
+﻿using Eight.Kyuu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EightKyu.Test
+{
+    public class ReturnToSanityTest
+    {
+        [Fact]
+        
+        public void ShouldReturnSanity()
+        {
+            var result = new Dictionary<string, string> { { "sanity", "hello" } };
+            var output = ReturnToSanity.Mystery();
+            Assert.Equal(result, output);
+
+        }
+    }
+}
