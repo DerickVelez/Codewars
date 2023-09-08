13b2bd04e3151a4582466dc28012e5eb9ef7d1c4
Jan Derick Velez <jderickvelez@gmail.com>
2023-09-08 22:08:19 +0800

    fixed test on FakeBinary

--- EightKyu.Test/FakeBinaryTest.cs
+++ EightKyu.Test/FakeBinaryTest.cs
@@ -10,7 +10,7 @@ namespace EightKyu.Test
     public class FakeBinaryTest
     {
         [Theory]
-        [InlineData("01011110001100111", "01011110001100111")]
+        [InlineData("00000000000000000", "01011110001100111")]
         [InlineData("011011110000101010000011011", "366058562030849490134388085")]
         [InlineData("101000111101101", "509321967506747")]
 
