9be5b490227cbf2998cd303218102211216c475f
Jan Derick Velez <jderickvelez@gmail.com>
2023-09-14 23:17:50 +0800

    Added Seven Kyu Kata: Area of a Circle

---
+++ Seven.Kyu/AreaOfaCircle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seven.Kyu
+{
+    public class AreaOfaCircle
+    {
+        public static double CalculateAreaOfCircle(double radius)
+        {
+            if (radius <= 0)
+                throw new System.ArgumentException("");
+            else
+                return 3.141592653589793238 * (radius * radius);
+        }
+    }
+}
---
+++ SevenKyu.Test/AreaOfaCircleTest.cs
@@ -0,0 +1,23 @@
+﻿using Seven.Kyu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SevenKyu.Test
+{
+    public class AreaOfaCircleTest
+    {
+        [Theory]
+        [InlineData(14526.724,68)]
+        [InlineData(43.2673, 5881.248)]
+
+        public void ShouldReturnAreaofCircle(double x, double y)
+        {
+            var result = AreaOfaCircle.CalculateAreaOfCircle(y);
+            Assert.Equal(x, result);
+        }
+    
+    }
+}
