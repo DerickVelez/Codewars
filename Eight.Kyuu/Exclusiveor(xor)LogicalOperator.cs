190c9add7c222eb3e0540fb8de2af53e80d00723
Jan Derick Velez <jderickvelez@gmail.com>
2023-09-23 15:18:24 +0800

    Added Eight Kyu Kata: Exclusive or (xor) Logical Operator

---
+++ Eight.Kyuu/Exclusiveor(xor)LogicalOperator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eight.Kyuu
+{
+    public class Exclusiveor_xor_LogicalOperator
+    {
+        public static bool Xor(bool a, bool b)
+        {
+            if (a == false && b == false || a == true && b == true)
+            { return false; }
+            else
+                return true;
+        }
+    }
+}
---
+++ EightKyu.Test/Exclusiveor_xor_LogicalOperatorTest.cs
@@ -0,0 +1,22 @@
+﻿using Eight.Kyuu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EightKyu.Test
+{
+    public class Exclusiveor_xor_LogicalOperatorTest
+    {
+        [Theory]
+        [InlineData(false, false, false)]
+        [InlineData(true, true, false)]
+
+        public void ShouldReturnTrueIfhasTrue(bool expected, bool a, bool b)
+        {
+            var result = Exclusiveor_xor_LogicalOperator.Xor(a, b);
+            Assert.Equal(expected, result);
+        }
+    }
+}
