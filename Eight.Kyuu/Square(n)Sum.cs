623ca0182576cd08bef2025982de235f406fe487
Jan Derick Velez <jderickvelez@gmail.com>
2023-09-27 23:30:59 +0800

    Added Eight Kyu Kata: Square(n) Sum

---
+++ Eight.Kyuu/Square(n)Sum.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eight.Kyuu
+{
+    public class Square_n_Sum
+    {
+        public static int SquareSum(int[] numbers)
+        {
+            List<int> nums = new List<int>();
+            foreach (var digit in numbers)
+            {
+                nums.Add(digit * digit);
+            }
+            return nums.Sum();
+        }
+    }
+}
---
+++ EightKyu.Test/Square_n_SumTest.cs
@@ -0,0 +1,25 @@
+﻿using Eight.Kyuu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EightKyu.Test
+{
+    public class Square_n_SumTest
+    {
+        [Theory]
+        [InlineData(9,new int[] { 1, 2, 2 })]
+        [InlineData(50, new int[] { 5, 3, 4 })]
+        [InlineData(5, new int[] { 1, 2 })]
+
+        public void ShouldReturnSquaredOfNums(int expected, int[] nums)
+        {
+            var result = Square_n_Sum.SquareSum(nums);
+            Assert.Equal(expected, result);
+             
+        }
+
+    }
+}
