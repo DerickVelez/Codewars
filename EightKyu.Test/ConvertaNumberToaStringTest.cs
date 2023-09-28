19976eb69731170c27000dd5d2412c0bb3be65bf
Jan Derick Velez <jderickvelez@gmail.com>
2023-09-28 22:32:25 +0800

    Added Eight Kyu Kata: Convert a Number to a String

---
+++ EightKyu.Test/ConvertaNumberToaStringTest.cs
@@ -0,0 +1,22 @@
+﻿using Eight.Kyuu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EightKyu.Test
+{
+    public class ConvertaNumberToaStringTest
+    {
+        [Theory]
+        [InlineData("67",67)]
+        [InlineData("1",1)]
+
+        public void ShouldReturnStringFormat(string expected, int actual)
+        {
+            var result = ConvertaNumberToaString.NumberToString(actual);
+            Assert.Equal(expected, result); 
+        }
+    }
+}
