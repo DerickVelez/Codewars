3d21a3b65768b85ac8650f49be3994345c48911b
Jan Derick Velez <jderickvelez@gmail.com>
2023-09-29 20:53:33 +0800

    Added seven Kyu Kata: Odd or Even

---
+++ Seven.Kyu/OddorEven.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seven.Kyu
+{
+    public class OddorEven
+    {
+        public static string OddOrEven(int[] array)
+        {
+            var result = array.Sum();
+            return (result % 2 == 0) ? "even" : "odd";
+        }
+    }
+}
---
+++ SevenKyu.Test/OddorEvenTest.cs
@@ -0,0 +1,24 @@
+﻿using Seven.Kyu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SevenKyu.Test
+{
+    public class OddorEvenTest
+    {
+        [Theory]
+        [InlineData("even",new int[] {0})]
+        [InlineData("even",new int[] {})]
+        [InlineData("odd", new int[] { 1023, 1, 3 })]
+
+        public void ShouldReturnOddorEven(string expected, int[] input)
+        {
+            var result = OddorEven.OddOrEven(input);
+            Assert.Equal(expected, result);
+        }
+
+    }
+}
