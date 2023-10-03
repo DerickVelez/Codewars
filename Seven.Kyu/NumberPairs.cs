beeda2e6b4f8c566fad76533ccf5335f0c86aed9
Jan Derick Velez <jderickvelez@gmail.com>
2023-10-03 22:37:31 +0800

    Added Seven Kyu Kata: Number paairs _Workinprogress

---
+++ Seven.Kyu/NumberPairs.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seven.Kyu
+{
+    public class NumberPairs
+    {
+        public static int[] getLargerNumbers(int[] a, int[] b)
+        {
+            int arrcount = a.Length;
+            var arrAll = a.Union(b);
+            var result = new List<int>();
+
+            for (var i = 0; i >= arrcount; i++)
+            {
+                result.Add(arrAll.Max());
+                Console.WriteLine(arrAll.Max());
+            }
+
+            return result.ToArray();
+        }
+    }
+}
