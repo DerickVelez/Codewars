a6ed28166fe48e92128a44f383ebf2de5a1ee1f6
Jan Derick Velez <jderickvelez@gmail.com>
2023-10-05 23:56:46 +0800

    Added Test Seven Kyu Kata: Beginner Series #5 Triangular Numbers

---
+++ SevenKyu.Test/BeginnerSeries5TriangularNumbersTest.cs
@@ -0,0 +1,22 @@
+﻿using Seven.Kyu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SevenKyu.Test
+{
+    public class BeginnerSeries5TriangularNumbersTest
+    {
+        [Theory]
+        [InlineData(true, 1)]
+        [InlineData(false, 2147483646)]
+
+        public void ShouldReturnTrueIfTriangleNumber(bool expected, int input)
+        {
+            var result = BeginnerSeries5TriangularNumbers.isTriangular(input);
+            Assert.Equal(expected, result);
+        }
+    }
+}
