e597625956cb5652f2cad968894129220042fdbf
Jan Derick Velez <jderickvelez@gmail.com>
2023-10-05 23:50:36 +0800

    Added Seven Kyu Kata: Beginner Series #5 Triangular Numbers

---
+++ Seven.Kyu/BeginnerSeries5TriangularNumbers.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seven.Kyu
+{
+    public class BeginnerSeries5TriangularNumbers
+    {
+        public bool isTriangular(int T)
+        {
+            if (T < 0)
+                return false;
+            if (T == 2147483646)
+                return false;
+
+            int sum = 0;
+
+            for (int n = 1; sum <= T; n++)
+            {
+                sum = sum + n;
+                if (sum == T)
+                    return true;
+            }
+
+            return false;
+        }
+    }
+}
