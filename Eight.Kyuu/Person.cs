cc87627f52d7340a86953df1fafb924d607303aa
Jan Derick Velez <jderickvelez@gmail.com>
2023-10-09 20:57:37 +0800

    Added Eight Kyu Kata: Person

---
+++ Eight.Kyuu/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eight.Kyuu
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+        public string Info => $"{name}s age is {age}";
+
+        public Person(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+    }
+}
