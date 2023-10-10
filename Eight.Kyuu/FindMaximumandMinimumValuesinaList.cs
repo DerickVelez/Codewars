03510e1da6e1984789b05cf1cecd51acba9cd40f
Jan Derick Velez <jderickvelez@gmail.com>
2023-10-10 17:46:21 +0800

    Added Eight Kyu Kata: Find Maximum and Minimum Value of a List

---
+++ Eight.Kyuu/FindMaximumandMinimumValuesinaList.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eight.Kyuu
+{
+    public class FindMaximumandMinimumValuesinaList
+    {
+            public static int Min(int[] list)
+             => list.Min();
+
+            public static int Max(int[] list)
+            => list.Max();
+    }
+}
---
+++ EightKyu.Test/FindMaximumandMinimumValuesinaListTest.cs
@@ -0,0 +1,25 @@
+﻿using Eight.Kyuu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EightKyu.Test
+{
+    public class FindMaximumandMinimumValuesinaListTest
+    {
+        [Theory]
+        [InlineData(new int[] { -52, 56, 30, 29, -54, 0, -110 }, 56, -110)]
+
+        public static void ShouldReturnMaxandMin(int[] input, int max, int min)
+        {
+
+            var maxResult = FindMaximumandMinimumValuesinaList.Max(input);
+            var minResult = FindMaximumandMinimumValuesinaList.Min(input);
+
+            Assert.Equal(max, maxResult);
+            Assert.Equal(min, minResult);
+        }
+    }
+}
