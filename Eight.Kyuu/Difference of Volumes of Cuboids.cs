d3b435e183143b31ed1216057c0053116c63456d
Jan Derick Velez <jderickvelez@gmail.com>
2023-10-25 23:27:50 +0800

    Added Eight Kyu Kata: Difference of Volumes of Cuboids

---
+++ Eight.Kyuu/Difference of Volumes of Cuboids.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eight.Kyuu
+{
+    internal class Difference_of_Volumes_of_Cuboids
+    {
+    }
+}
---
+++ EightKyu.Test/Difference_of_Volumes_of_Cuboids_Test.cs
@@ -0,0 +1,23 @@
+﻿using Eight.Kyuu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace EightKyu.Test
+{
+    public class Difference_of_Volumes_of_Cuboids_Test
+    {
+        [Theory]
+        [InlineData( new int[] { 3, 2, 5 }, new int[] { 1, 4, 4 },14)]
+        [InlineData(new int[] {9, 7, 2}, new int[] { 5, 2, 2 },106)]
+
+        public void ShouldReturnDifferenceofCuboids(int[] arrA, int[] arrB, int expected)
+        {
+            var result = Difference_of_Volumes_of_Cuboids.FindDifference(arrA, arrB);
+            Assert.Equal(expected, result);
+        }
+    }
+}
