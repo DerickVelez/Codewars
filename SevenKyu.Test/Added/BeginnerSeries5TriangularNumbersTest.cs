a466268f9d6562e0db5476eeb293ccfaa6dd045c
Jan Derick Velez <jderickvelez@gmail.com>
2023-11-01 23:49:45 +0800

    fixed Test: Begginer Series  Triangular Number Test

--- SevenKyu.Test/Added/BeginnerSeries5TriangularNumbersTest.cs
+++ SevenKyu.Test/Added/BeginnerSeries5TriangularNumbersTest.cs
@@ -14,6 +14,8 @@ namespace SevenKyu.Test.Added
         [InlineData(false, 0)]
         [InlineData(true, 6)]
         [InlineData(true, 10)]
+        [InlineData(true, 15)]
+
 
 
         public void ShouldReturnTrueIfTriangleNumber(bool expected, int input)
