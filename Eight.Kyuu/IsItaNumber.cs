946e3e8c9f0062b2c8339b865344367e37b663de
Jan Derick Velez <jderickvelez@gmail.com>
2023-11-20 23:01:01 +0800

    Added Eight Kyu Kata: Is It a number_ In Progress

---
+++ Eight.Kyuu/IsItaNumber.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eight.Kyuu
+{
+    public class IsItaNumber
+    {
+        public static bool IsDigit(string s)
+        {
+            bool correct = true;
+            bool Isnumerical = int.TryParse(s, out correct);
+
+            return Isnumerical;
+        }
+    }
+}
