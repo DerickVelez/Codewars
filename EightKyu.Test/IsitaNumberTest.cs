e19420db1cdaa74042e6059b8fe8820bb3c9dc4c
Jan Derick Velez <jderickvelez@gmail.com>
2023-11-22 21:20:58 +0800

    Added Test Eight Kyu Kata: Is it a Number

---
+++ EightKyu.Test/IsitaNumberTest.cs
@@ -0,0 +1,23 @@
+﻿using Eight.Kyuu;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EightKyu.Test
+{
+    public class IsitaNumberTest
+    {
+        [Theory]
+        [InlineData("234",true)]
+        [InlineData(";askdf",false)]
+
+        public void ShouldReturnTrueIfNumber(string input, bool expected)
+        {
+            var result = IsItaNumber.IsDigit(input);
+            Assert.Equal(expected, result);
+        }
+    }
+}
