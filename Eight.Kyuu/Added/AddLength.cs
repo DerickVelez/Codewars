651d15e67c2d8fd45c257b5afc84bc2034cb73ae
Jan Derick Velez <jderickvelez@gmail.com>
2023-11-24 23:51:42 +0800

    Added Eight Kyu Kata: Is is a Number

--- Eight.Kyuu/Added/AddLength.cs
+++ Eight.Kyuu/Added/AddLength.cs
@@ -8,17 +8,17 @@ namespace Eight.Kyuu.Added
 {
     public class AddLength
     {
-        public static bool IsDigit(string s)
-        {
-            var correct;
-            bool Isnumerical = int.TryParse(s, out correct);
+        //public static bool IsDigit(string s)
+        //{
+        //    var correct;
+        //    bool Isnumerical = int.TryParse(s, out correct);
 
-            if (Isnumerical == true)
-            { return true; }
-            else
-            { }
+        //    if (Isnumerical == true)
+        //    { return true; }
+        //    else
+        //    { }
 
-            return Isnumerical;
+        //    return Isnumerical;
         }
     }
 }
