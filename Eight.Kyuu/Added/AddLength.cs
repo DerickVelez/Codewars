9aac481ae0cb20ba9ea278eb68d4a9c33984840b
Jan Derick Velez <jderickvelez@gmail.com>
2023-11-25 21:50:26 +0800

    temporarily hold Add length Kata

--- Eight.Kyuu/Added/AddLength.cs
+++ Eight.Kyuu/Added/AddLength.cs
@@ -4,21 +4,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Eight.Kyuu.Added
-{
-    public class AddLength
-    {
-        //public static bool IsDigit(string s)
-        //{
-        //    var correct;
-        //    bool Isnumerical = int.TryParse(s, out correct);
+//namespace Eight.Kyuu.Added
+//{
+//    public class AddLength
+//    {
+//        //public static bool IsDigit(string s)
+//        //{
+//        //    var correct;
+//        //    bool Isnumerical = int.TryParse(s, out correct);
 
-        //    if (Isnumerical == true)
-        //    { return true; }
-        //    else
-        //    { }
+//        //    if (Isnumerical == true)
+//        //    { return true; }
+//        //    else
+//        //    { }
 
-        //    return Isnumerical;
-        }
-    }
-}
+//        //    return Isnumerical;
+//        }
+//    }
+//}
--- EightKyu.Test/AddedTest/AddLenghtTest.cs
+++ EightKyu.Test/AddedTest/AddLenghtTest.cs
@@ -5,18 +5,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace EightKyu.Test.AddedTest
-{
-    public class AddLenghtTest
-    {
-        [Theory]
-        [InlineData(new string[] { "apple 5", "ban 3" }, "apple ban")]
-        [InlineData(new string[] { "you 3", "will 4", "win 3" }, "you will win")]
+//namespace EightKyu.Test.AddedTest
+//{
+//    public class AddLenghtTest
+//    {
+//        [Theory]
+//        [InlineData(new string[] { "apple 5", "ban 3" }, "apple ban")]
+//        [InlineData(new string[] { "you 3", "will 4", "win 3" }, "you will win")]
 
-        public void ShouldReturnStringWithLength(string[] expected, string input)
-        {
-            var result = AddLength.AddLengthInString(input);
-            Assert.Equal(expected, result);
-        }
-    }
-}
+//        public void ShouldReturnStringWithLength(string[] expected, string input)
+//        {
+//            var result = AddLength.AddLengthInString(input);
+//            Assert.Equal(expected, result);
+//        }
+//    }
+//}
