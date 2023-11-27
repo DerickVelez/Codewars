6ef3592b6fc083beb25028e03237766771330a1e
Jan Derick Velez <jderickvelez@gmail.com>
2023-11-27 17:45:37 +0800

    Added Eight Kyu Kata: Grasshopper - Array Mean

---
+++ Eight.Kyuu/Grasshopper-ArrayMean.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eight.Kyuu
+{
+    public class Grasshopper_ArrayMean
+    {
+        public static int FindAverage(int[] nums)
+        {
+            return (int)nums.Average();
+        }
+    }
+}
