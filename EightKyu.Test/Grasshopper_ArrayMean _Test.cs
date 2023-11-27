091878b738d5be6522037fb85fb7c73e2f219778
Jan Derick Velez <jderickvelez@gmail.com>
2023-11-27 17:53:08 +0800

    Added Eight Kyu Kata: Test case for Grasshopper Array Mean

---
+++ EightKyu.Test/Grasshopper_ArrayMean _Test.cs
@@ -0,0 +1,24 @@
+﻿using Eight.Kyuu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EightKyu.Test
+{
+    public class Grasshopper_ArrayMean__Test
+    {
+        [Theory]
+        [InlineData(new[] { 1 },1)]
+        [InlineData(new[] { 1, 3, 5, 7 },4)]
+        [InlineData(new[] { -10, -5, -5, 0 },-5)]
+
+
+        public void ShouldReturnAverage(int[] actual, int expected)
+        {
+            var result = Grasshopper_ArrayMean.FindAverage(actual);]
+            Assert.Equal(expected, result);
+        }
+    }
+}
