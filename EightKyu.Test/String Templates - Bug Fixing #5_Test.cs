87d7ac4329a270217299b3234735e1d8abb8fef1
Jan Derick Velez <jderickvelez@gmail.com>
2023-11-28 21:22:18 +0800

    Added Eight Kyu Kata: Added test on String Templates - Bug Fixing #5

---
+++ EightKyu.Test/String Templates - Bug Fixing #5_Test.cs
@@ -0,0 +1,22 @@
+﻿using Eight.Kyuu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EightKyu.Test
+{
+    public class String_Templates___Bug_Fixing__5_Test
+    {
+        [Theory]
+        [InlineData("I like Cheese, Milk, Chocolate!", new string[] { "Cheese", "Milk", "Chocolate")]
+        [InlineData("I like Cheese, Milk!", new string[] { "Cheese", "Milk" })]
+
+        public void ShouldReturnCorrectedString(string expected, string[] actual)
+        {
+            var result = String_Templates___Bug_Fixing__5.buildString(actual); 
+            Assert.Equal(expected, result);
+        }
+    }
+}
