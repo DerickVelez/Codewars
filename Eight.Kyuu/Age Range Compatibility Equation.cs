5d0365e2b9c70c8d42f6c9f6eb58e02d748c0396
Jan Derick Velez <jderickvelez@gmail.com>
2023-11-30 16:57:17 +0800

    Added Eight Kyu Kata: Age Range Compatibility

---
+++ Eight.Kyuu/Age Range Compatibility Equation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eight.Kyuu
+{
+    public class Age_Range_Compatibility_Equation
+    {
+        public static string DatingRange(int age)
+        {
+            var min = 0;
+            var max = 0;
+
+            if (age <= 14)
+            {
+                min = (int)Math.Floor(age - 0.10 * age);
+                max = (int)Math.Floor(age + 0.10 * age);
+            }
+            else
+            {
+                min = (age / 2) + 7;
+                max = (age - 7) * 2;
+            }
+            return $"{min}-{max}";
+        }
+    }
+}
