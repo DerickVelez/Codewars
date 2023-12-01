943ff50ea8a520614c67637c9bd358907cfe4ca6
Jan Derick Velez <jderickvelez@gmail.com>
2023-12-01 23:33:05 +0800

    Added Eight Kyu Kata: Age_Range_Compatibility_Equation_Test

---
+++ EightKyu.Test/Age Range Compatibility Equation_Test.cs
@@ -0,0 +1,22 @@
+﻿using Eight.Kyuu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace EightKyu.Test
+{
+    public class Age_Range_Compatibility_Equation_Test
+    {
+        [Theory]
+        [InlineData("15-20",17)]
+
+        public void ShouldReturnRange(string expected, int given)
+        {
+            var result = Age_Range_Compatibility_Equation.DatingRange(given);
+            Assert.Equal(expected, result);
+        }
+    }
+}
