8be5f26fb20310e98000f2eec7a60a31baad35b9
Jan Derick Velez <jderickvelez@gmail.com>
2023-12-14 16:49:10 +0800

    Added Eight Kyu Kata: Regexp Basics - is it a digit?

---
+++ Eight.Kyuu/Regexp Basics - is it a digit.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Eight.Kyuu
+{
+    public static class Regexp_Basics___is_it_a_digit
+    {
+        public static bool Digit(this string s)
+        {
+
+            string pattern = @"^-?[0-9][0-9,\.]+$";
+
+            var answer = Regex.IsMatch(s, pattern);
+            if (answer == true)
+            {
+
+                return true;
+            }
+            else
+                Console.WriteLine(s);
+            return false;
+        }
+    }
+}
