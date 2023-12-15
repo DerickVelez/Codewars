e152b5b74eb8353fc30c098438b4fe11c4b9f9bd
Jan Derick Velez <jderickvelez@gmail.com>
2023-12-15 22:26:23 +0800

    Added Eight Kyu Kata: Regexp_Basics___is_it_a_digit_Test

---
+++ EightKyu.Test/Regexp_Basics___is_it_a_digit_Test.cs
@@ -0,0 +1,25 @@
+﻿using Eight.Kyuu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EightKyu.Test
+{
+    public class Regexp_Basics___is_it_a_digit_Test
+    {
+        [Theory]
+        [InlineData("",false)]
+        [InlineData("0",true)]
+        [InlineData(" ",false)]
+        [InlineData("12",true)]
+
+        public void ShouldReturnTrueifNumber(string input,  bool expected)
+        {
+            var result = Regexp_Basics___is_it_a_digit.Digit(input);
+            Assert.Equal(expected, result);
+        }
+
+    }
+}
