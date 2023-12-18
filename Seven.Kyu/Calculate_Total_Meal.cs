c5825b1c8eb720c4109b01509bcaceaa7e91eb04
Jan Derick Velez <jderickvelez@gmail.com>
2023-12-18 23:02:23 +0800

    Added Seven Kyu Kata: Calculate Total Meal

---
+++ Seven.Kyu/Calculate_Total_Meal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seven.Kyu
+{
+    public class Calculate_Total_Meal
+    {
+        public static double CalculateTotal(double subtotal, int tax, int tip)
+        {
+            var taxedTotal = (tax * 0.01) * subtotal;
+            var tipTotal = (tip * 0.01) * subtotal;
+            return Math.Round(taxedTotal + tipTotal + subtotal, 2);
+        }
+    }
+}
