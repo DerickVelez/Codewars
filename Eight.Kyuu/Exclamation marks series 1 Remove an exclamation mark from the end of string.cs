8ff6a8d3beb58bd22c2a79fbf4e14b58ed66031c
Jan Derick Velez <jderickvelez@gmail.com>
2023-12-27 23:37:45 +0800

    Added Eight Kyu Kata: Exclamation marks series #1: Remove an exclamation mark from the end of string

---
+++ Eight.Kyuu/Exclamation marks series 1 Remove an exclamation mark from the end of string.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eight.Kyuu
+{
+    public class Exclamation_marks_series_1_Remove_an_exclamation_mark_from_the_end_of_string
+    {
+        public static string Remove(string s)
+        {
+            if (s.EndsWith("!"))
+                return s.Substring(0, s.Length - 1);
+            return s;
+        }
+    }
+}
