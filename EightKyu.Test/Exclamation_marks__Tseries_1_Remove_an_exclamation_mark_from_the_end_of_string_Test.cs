72433deb52993b832a5bcf14855f57e3d041f19f
Jan Derick Velez <jderickvelez@gmail.com>
2023-12-27 23:42:41 +0800

    Added Eight Kyu Kata: Exclamation_marks_series_1_Remove_an_exclamation_mark_from_the_end_of_string_Test

---
+++ EightKyu.Test/Exclamation_marks__Tseries_1_Remove_an_exclamation_mark_from_the_end_of_string_Test.cs
@@ -0,0 +1,21 @@
+﻿using Eight.Kyuu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EightKyu.Test
+{
+    public class Exclamation_marks__Tseries_1_Remove_an_exclamation_mark_from_the_end_of_string_Test
+    {
+        [Theory]
+        [InlineData("Hi", "Hi!")]
+        [InlineData("Hi!!", "Hi!!!")]
+
+        public void ShouldReturnReducedString(string expected, string actual)
+        {
+            var result = Exclamation_marks_series_1_Remove_an_exclamation_mark_from_the_end_of_string.Remove(expected);
+            Assert.Equal(actual, result);
+        }
+    }
