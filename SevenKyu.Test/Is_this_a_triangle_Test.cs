eaa4b53cd06153b9a62cb173780d8dc314d1f171
Jan Derick Velez <jderickvelez@gmail.com>
2024-01-03 17:23:39 +0800

    Added  Seven Kyu Kata test: Is this a triangle test

---
+++ SevenKyu.Test/Is_this_a_triangle_Test.cs
@@ -0,0 +1,23 @@
+﻿using Seven.Kyu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SevenKyu.Test
+{
+    public class Is_this_a_triangle_Test
+    {
+        [Theory]
+        [InlineData(true,5, 7, 10)]
+        [InlineData(false, 1, 2, -3)]
+        [InlineData(false, 1, 4, 2)]
+
+        public void ShouldReturnTrueIfTriangle(bool expected, int a, int b, int c)
+        {
+            var result = Is_this_a_triangle.IsTriangle(a, b, c);
+            Assert.Equal(expected, result);
+        }
+    }
+}
