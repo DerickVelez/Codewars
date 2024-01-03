40c5122b619dee85ed2ee6b6e5640f674dd956f2
Jan Derick Velez <jderickvelez@gmail.com>
2024-01-03 17:17:25 +0800

    Added Sevem Kyu Kata: Is this a triangle?

---
+++ Seven.Kyu/Is this a triangle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seven.Kyu
+{
+    public class Is_this_a_triangle
+    {
+        public static bool IsTriangle(int a, int b, int c)
+        {
+            var maxnum = new[] { a, b, c }.Max();
+            var min1 = new[] { a, b, c }.Min();
+            var min2 = new[] { a, b, c }.Sum() - maxnum - min1;
+
+            if (a == b && a == c)
+            { return true; }
+            if (a < 0 || b < 0 || c < 0)
+            { return false; }
+            if (min1 + min2 <= maxnum)
+            { return false; }
+            else
+            { return true; }
+        }
+    }
+}
