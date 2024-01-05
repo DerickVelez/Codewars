110965d6b8a685c450311751650330c8643ab58e
Jan Derick Velez <jderickvelez@gmail.com>
2024-01-05 12:40:46 +0800

    Added Seven Kyu Kata: Count the divisors of a number

---
+++ Seven.Kyu/Count the divisors of a number.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seven.Kyu
+{
+    public   class Count_the_divisors_of_a_number
+    {
+
+        //Cannot handle big numbers
+        public static int Divisors(int n)
+        {
+            int count = 0;
+
+            for (int i = n; i > 0; i--)
+            {
+                if (n % i == 0)
+                {
+                    count++;
+                }
+                else
+                { Console.WriteLine("nothing"); }
+            }
+            return count;
+        }
+    }
+}
