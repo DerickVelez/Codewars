414618ca0cbca2cf231fa26bbe6767f2babed675
Jan Derick Velez <jderickvelez@gmail.com>
2024-01-05 12:44:33 +0800

    Added Seven Kyu Kata Test: Count the number of divisors Test

---
+++ SevenKyu.Test/Count_the_divisors_of_a_number_Test.cs
@@ -0,0 +1,24 @@
+﻿using Seven.Kyu;
+using Seven.Kyu.Added;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SevenKyu.Test
+{
+    public class Count_the_divisors_of_a_number_Test
+    {
+        [Theory]
+        [InlineData(1,1)]
+        [InlineData(4,10)]
+        [InlineData(8,54)]
+
+        public void ShouldReturnNumberofDivisor(int expected, int n)
+        {
+            var result = Count_the_divisors_of_a_number.Divisors(n);
+            Assert.Equal(expected, result);
+        }
+    }
+}
