846a707d96aea2a6bd12ab81b263636678f16fe5
Jan Derick Velez <jderickvelez@gmail.com>
2024-02-13 23:02:03 +0800

    Added Seven Kyu Kata: Shortest Word

---
+++ SevenKyu.Test/Shortest Word Test.cs
@@ -0,0 +1,22 @@
+﻿using Seven.Kyu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SevenKyu.Test
+{
+    public class Shortest_Word_Test
+    {
+        [Theory]
+        [InlineData(3, "bitcoin take over the world maybe who knows perhaps")]
+        [InlineData(2, "Let's travel abroad shall we")]
+
+        public void ReturnShortestWordNum(int expected,  string word)
+        {
+            var result = Shortest_Word.FindShort(word);
+            Assert.Equal(expected, result);
+        }
+    }
+}
