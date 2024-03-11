4aad8e7b573bdefdef006234a5a827ccbe1cf9ce
Jan Derick Velez <jderickvelez@gmail.com>
2024-03-11 23:33:47 +0800

    Added Eight  Kyu Kata: Fundamentals Return

---
+++ Eight.Kyuu/Fundamentals Return.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eight.Kyuu
+{
+    using System;
+public static class Kata
+    {
+        public static int Add(int a, int b)
+        {
+            var result = a + b;
+            return result;
+        }
+
+        public static int Multiply(int a, int b)
+        {
+            var result = a * b;
+            return result;
+        }
+
+        public static int Divide(int a, int b)
+        {
+            var result = a / b;
+            return result;
+        }
+
+        public static int Mod(int a, int b)
+        {
+            var result = a % b;
+            return result;
+        }
+
+        public static int Exponent(int a, int b)
+        {
+            var result = Math.Pow(a, b);
+            return (int)result;
+        }
+
+        public static int Subt(int a, int b)
+        {
+            var result = a - b;
+            return result;
+        }
+        // Add more functions Here. Refer to the description for function names.
+        // The code will NOT WORK IF YOU USE names other than the ones
+        // from the description
+
+    }
+}
+
+
