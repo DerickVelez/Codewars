1b6bf53c7eaec0b3abbb6641c1738d885162904e
Jan Derick Velez <jderickvelez@gmail.com>
2024-03-15 23:09:11 +0800

    Added Eight Kyu Kata: Fundamentals Return

---
+++ EightKyu.Test/Fundamentals Return Test.cs
@@ -0,0 +1,22 @@
+﻿using Eight.Kyuu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EightKyu.Test
+{
+    public class Fundamentals_Return_Test
+    {
+        [Theory]
+        [InlineData(12,5,7)]
+
+        public void ShouldReturnREsult(int expected, int a, int b)
+        {
+            var result = Kata.Add( a, b);
+            Assert.Equal(expected, result);
+
+        }
+    }
+}
